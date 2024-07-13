b45edac168ec05067e99d6b9a1b8e16062be1b9a
mrlima4095 <felipebr4095@gmail.com>
2024-07-13 08:49:30 -0300

    Add login function
    I'm working to add to OpenTTY a login method, now I implement commands [login, logout and whoami].

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -7,16 +7,22 @@ public class OpenTTY extends MIDlet implements CommandListener {
     private TextField commandInput;
     private StringItem output;
     private Command enterCommand;
+    private String username;
+    private String version;
     private boolean app;
 
     public void startApp() {
+        
         if (!app == true) {
+            // OpenTTY Settings
+            version = "1.1";
+            username = "";
         
             display = Display.getDisplay(this);
-            form = new Form("OpenTTY 1.0");
+            form = new Form("OpenTTY " + version);
             
             commandInput = new TextField("Command", "", 256, TextField.ANY);
-            output = new StringItem("", "Welcome to OpenTTY 1.0\nCopyright (C) 2024 - Mr. Lima\n");
+            output = new StringItem("", "Welcome to OpenTTY " + version + "\nCopyright (C) 2024 - Mr. Lima\n");
             
             enterCommand = new Command("Send", Command.OK, 1);
             
@@ -50,19 +56,22 @@ public class OpenTTY extends MIDlet implements CommandListener {
         if (mainCommand.equals("")) { } 
         else if (mainCommand.equals("!")) { echoCommand("OpenTTY Java Edition"); }
         else if (mainCommand.equals("date")) { dateCommand(); } 
+        else if (mainCommand.equals("login")) { login(argument); }
         else if (mainCommand.equals("exit")) { notifyDestroyed(); } 
         else if (mainCommand.equals("call")) { callCommand(argument); } 
         else if (mainCommand.equals("echo")) { echoCommand(argument); }
         else if (mainCommand.equals("open")) { openCommand(argument); }
         else if (mainCommand.equals("uname")) { unameCommand(argument); }
         else if (mainCommand.equals("execute")) { processCommand(argument); }
-        else if (mainCommand.equals("version")) { echoCommand("OpenTTY 1.0"); }
+        else if (mainCommand.equals("version")) { echoCommand("OpenTTY " + version); }
         else if (mainCommand.equals("clear") || mainCommand.equals("cls")) { output.setText(""); } 
         else if (mainCommand.equals("locale")) { echoCommand(System.getProperty("microedition.locale")); }
         else if (mainCommand.equals("hostname")) { echoCommand(System.getProperty("microedition.hostname")); } 
-        else if (mainCommand.equals("title")) { if (argument.equals("") ) { form.setTitle("OpenTTY 1.0"); } else {form.setTitle(argument); } }
-        else if (mainCommand.equals("sh")) { form.setTitle("OpenTTY 1.0"); output.setText("Welcome to OpenTTY 1.0\nCopyright (C) 2024 - Mr. Lima\n"); }
-        else if (mainCommand.equals("help")) {echoCommand("[call] [clear] [date]\n[echo] [uname] [exit]\n[title] [version] [!]\n[execute]  [hostname]2"); }
+        else if (mainCommand.equals("logout")) { if (username.equals("")) { echoCommand("logout: you aren't logged"); } else { username = ""; }  } 
+        else if (mainCommand.equals("title")) { if (argument.equals("") ) { form.setTitle("OpenTTY" + version); } else {form.setTitle(argument); } }
+        else if (mainCommand.equals("sh")) { form.setTitle("OpenTTY " + version); output.setText("Welcome to OpenTTY " + version + "\nCopyright (C) 2024 - Mr. Lima\n"); }
+        else if (mainCommand.equals("whoami")) { if (username.equals("")) { echoCommand("whoami: you aren't logged"); } else { echoCommand(username + "@" + System.getProperty("microedition.hostname")); } } 
+        else if (mainCommand.equals("help")) { echoCommand("[call] [clear] [date]\n[echo] [uname] [exit]\n[title] [version] [!]\n[execute]  [hostname]"); }
         else { output.setText(output.getText() + "\n" + mainCommand + ": unknown command"); }
         
     }
@@ -77,7 +86,6 @@ public class OpenTTY extends MIDlet implements CommandListener {
         if (spaceIndex == -1) { return ""; } else { return input.substring(spaceIndex + 1).trim(); }
     }
     
-    // OpenTTY - Class to command [date]
     private void dateCommand() {
         java.util.Date date = new java.util.Date();
         String dateString = date.toString();
@@ -89,5 +97,6 @@ public class OpenTTY extends MIDlet implements CommandListener {
     private void unameCommand(String options) { output.setText(output.getText() + "\n" + System.getProperty("microedition.platform") + " " + System.getProperty("microedition.configuration") + " " + System.getProperty("microedition.profiles")); }
     private void callCommand(String number) { if (number == null || number.length() == 0) { echoCommand("Usage: call <phone>"); return; } try { platformRequest("tel:" + number); } catch (Exception e) { } }
     private void openCommand(String url) { if (url == null || url.length() == 0) { echoCommand("Usage: open <url>"); return; } try { platformRequest(url); } catch (Exception e) { echoCommand("open: " + url + ": not found"); } }
+    private void login(String user) { if (user == null || user.length() == 0) { echoCommand("Usage: login <username>"); } else { if (username.equals("")) { username = user; } else { echoCommand("login: you are alredy login"); } } }
     
 }
