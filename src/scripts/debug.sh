d04370c6834b1b325848f3bbe27ffb4f1ea2fb75
Mr. Lima <felipebr4095@gmail.com>
2024-08-17 10:27:38 -0300

    The release is comming

--- src/scripts/debug.sh
+++ src/scripts/debug.sh
@@ -4,12 +4,17 @@ echo OpenTTY Debug
 echo -------------------
 echo
 
-tty
-ttysize
+basename /java/bin/basename
+locale
 date
 hostname
 whoami
 uname
 echo PATH=$PATH
+if $PATH / echo you are in root directory
+if not $USERNAME echo you are not logged
+!
+tty
+ttysize
 
 echo -------------------
