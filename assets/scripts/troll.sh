4ba4f40192b28d5c5202f4a6056fb3549dd9687d
Mr. Lima <felipebr4095@gmail.com>
2024-08-23 22:25:27 -0300

    a

---
+++ assets/scripts/troll.sh
@@ -0,0 +1,19 @@
+#!/java/bin/sh
+# -*- coding: utf-8 -*-
+#    
+#    . /scripts/troll.sh 
+#    
+#  Copyright (C) 2024 "Mr. Lima"
+#  
+# Use this script to troll your friends
+# Installing this script at initd file 
+# If you want fuck all installation of them
+# Cut the end lines that replace the code by
+# a blank content
+
+xorg stop
+warn [You have been trolled] $lThis prank script shutdown the OpenTTY Interface when you starts the MIDlet. 
+
+# Repair the prank
+rnano
+install initd
