f0cddb77693752e7ed7ca6037531dc477b03f205
Mr. Lima <felipebr4095@gmail.com>
2024-10-28 17:38:48 -0300

    cmd

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -173,6 +173,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("import")) { importScript(argument); }
 
         else if (mainCommand.equals("github")) { openCommand(getAppProperty("MIDlet-Info-URL")); }
+        else if (mainCommand.equals("")) { display.setCurrent(new FileExplorer(display)); }
         //else if (mainCommand.equals("")) {  }
 
         else if (mainCommand.equals("!")) { echoCommand(env("main/$RELEASE"));  }
