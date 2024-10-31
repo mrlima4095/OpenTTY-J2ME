b8755567f1be487b6a4ba425c11b7ec6c0a7b7f1
Mr. Lima <felipebr4095@gmail.com>
2024-10-31 18:20:58 -0300

    g

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -228,16 +228,9 @@ public class OpenTTY extends MIDlet implements CommandListener {
     private void install(String filename) { if (filename == null || filename.length() == 0) { return; } writeRMS(filename, nanoContent); }
     private void explorer() { final List files = new List(form.getTitle(), List.IMPLICIT); final Command open = new Command("Open", Command.OK, 2); final Command delete = new Command("Delete", Command.OK, 3); final Command run = new Command("Run Script", Command.OK, 4); final Command importfile = new Command("Import File", Command.OK, 5); try { String[] recordStores = RecordStore.listRecordStores(); if (recordStores != null) { for (int i = 0; i < recordStores.length; i++) { files.append((String) recordStores[i], null); } } } catch (RecordStoreException e) { } files.addCommand(new Command("Back", Command.BACK, 1)); files.addCommand(open); files.addCommand(delete); files.addCommand(run); files.addCommand(importfile); files.setCommandListener(new CommandListener() { public void commandAction(Command c, Displayable d) { if (c.getCommandType() == Command.BACK) { display.setCurrent(form); } else if (c == delete) { deleteFile(files.getString(files.getSelectedIndex())); explorer(); } else if (c == open) { nano(files.getString(files.getSelectedIndex())); } else if (c == run) { display.setCurrent(form); processCommand("run " + files.getString(files.getSelectedIndex())); } else if (c == importfile) { display.setCurrent(form); importScript(files.getString(files.getSelectedIndex())); } } }); display.setCurrent(files); }
 
-    private void calendar() {
-        final Form cal = new Form(form.getTitle());
-
-        cal.append(new DateField(null , DateField.DATE));
-        cal.addCommand(new Command("Back", Command.BACK, 1));
-        cal.setCommandListener(this); display.setCurrent(cal);
+    private void calendar() { final Form cal = new Form(form.getTitle()); cal.append(new DateField(null , DateField.DATE)); cal.addCommand(new Command("Back", Command.BACK, 1)); cal.setCommandListener(this); display.setCurrent(cal); }
 
 
-    }
- 
     // MIDlet Services Command Processor 
     private void xserver(String command) {
         command = env(command.trim());
