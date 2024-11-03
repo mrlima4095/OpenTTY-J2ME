befef40099cd84fe38bce8dd0ff33019169b0b77
Mr. Lima <felipebr4095@gmail.com>
2024-11-03 14:14:07 -0300

    i12

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -291,6 +291,8 @@ public class OpenTTY extends MIDlet implements CommandListener {
         public J2Buster(String url) {
             if (url == null || url.length() == 0) { return; }
 
+            this.url = url
+
             List pages = new List("J2Buster (" + url + ")", List.IMPLICIT);
             String[] wordlist = split(loadRMS("j2buster"), '\n');
 
