f1b31a4813b0272b41914e04623fa3a757fe1007
Mr. Lima <felipebr4095@gmail.com>
2024-11-11 11:55:19 -0300

    bool are

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -156,7 +156,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("open")) { if (argument.equals("")) { } else { try { platformRequest(argument); } catch (Exception e) { echoCommand("open: " + argument + ": not found"); } } }
         else if (mainCommand.equals("pkg")) { echoCommand(argument.equals("") ? getAppProperty("MIDlet-Name") : getAppProperty(argument)); }
         else if (mainCommand.equals("run")) { if (argument.equals("")) { runScript(nanoContent); } else { runScript(loadRMS(argument, 1)); } }
-        else if (mainCommand.equals("reset")) { try { long alarmTime = System.currentTimeMillis() + argument.equals("") ? 5000 : Integer.parseInt(argument) * 1000; PushRegistry.registerAlarm(getClass().getName(), alarmTime); processCommand("exit"); } catch (Exception e) { echoCommand("AutoRunError: " + e.getMessage()); } }
+        else if (mainCommand.equals("reset")) { try { long alarmTime = System.currentTimeMillis() + argument.length() == 0 ? 5000 : Integer.parseInt(argument) * 1000; PushRegistry.registerAlarm(getClass().getName(), alarmTime); processCommand("exit"); } catch (Exception e) { echoCommand("AutoRunError: " + e.getMessage()); } }
         else if (mainCommand.equals("sleep")) { if (argument.equals("")) { } else { try { Thread.sleep(Integer.parseInt(argument) * 1000); } catch (InterruptedException e) { } catch (IOException e) { echoCommand(e.getMessage()); } } }
         else if (mainCommand.equals("seed")) { echoCommand("" +  random.nextInt(999) + ""); }
         else if (mainCommand.equals("set")) { setCommand(argument); }
