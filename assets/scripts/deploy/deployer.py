46b3ddaad802a950002fd7595a68dbfb56db3bbf
Mr. Lima <felipebr4095@gmail.com>
2024-12-12 11:38:56 -0300

    Deploy tools

---
+++ assets/scripts/deploy/deployer.py
@@ -0,0 +1,20 @@
+import socket
+import sys, time
+
+host = "192.168.1.14"
+port = 31522
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+try: filename = sys.argv[1]
+except IndexError: print("missing file"), sys.exit(0)
+
+with open(filename, "rt") as file:
+    file = file.readlines()
+
+    for line in file:
+        s.send(line.encode())
+        time.sleep(3)
+
+    print("End of File")
