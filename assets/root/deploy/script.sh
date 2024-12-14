7f10f2d981a70a4331b6769d06b5275359df74e5
Mr. Lima <felipebr4095@gmail.com>
2024-12-14 10:16:59 -0300

    New 'deployer.py'

---
+++ assets/root/deploy/script.sh
@@ -0,0 +1,37 @@
+tick Debugging...
+install nano
+touch
+clear
+start deployer
+echo Starting OpenTTY Deploying\n\n
+
+about /java/bin/sh
+basename /java/lib/yang
+build
+date
+
+dir v
+alias
+env
+
+fw
+gaddr
+hostname
+genip
+
+debug
+
+add key=value
+json
+pjnc
+
+@alert
+@flash
+
+stop deployer
+tick
+
+get nano
+clear
+
+gc
