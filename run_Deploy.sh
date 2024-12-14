34b435d0330a13d243bf232646ef8267b4e2cb66
Mr. Lima <felipebr4095@gmail.com>
2024-12-14 10:17:29 -0300

    Scripts

---
+++ run_Deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Script to start OpenTTY Server
+# Python 3.8.x or newer required
+
+
+cd assets/root/deploy
+
+python deployer.py script.sh
+
---
+++ run_Server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Script to start OpenTTY Server
+# Python 3.8.x or newer required
+
+
+cd assets
+
+python server.py 
