c089b27c6d4af1ae49f83abcaf1792c0d7c347be
Mr. Lima <felipebr4095@gmail.com>
2024-12-16 21:07:15 -0300

    Improving package for dist 'REMOTE'

--- assets/dist/remote/java/etc/initd.sh
+++ assets/dist/remote/java/etc/initd.sh
@@ -4,25 +4,10 @@
 #    . /java/etc/initd.sh 
 #    
 
-set TTY=/java/optty1
+@login remote-pkg-client
 
-set HOSTNAME=localhost
-set RESPONSE=/java/etc/index.html
+alias xterm=execute /java/lib/remote; true 
 
-set PORT=31522
-set QUERY=nano
-
-
-mount /java/etc/fstab
-
-touch
-add quest.title=Remote
-add quest.label=IP Adress
-add quest.cmd=nc $ADDRESS
-add quest.key=ADDRESS
-
-alias xterm=execute exit; true 
-
-x11 quest nano
+x11 quest remote
 gc
 
