95513b9f0968bd838167ac8a80a80691ff0f30c1
Mr. Lima <felipebr4095@gmail.com>
2025-02-01 10:08:55 -0300

    Command 'CHMOD'

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -193,7 +193,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("import")) { importScript(argument); }
 
         //else if (mainCommand.equals("")) {  }
-        else if (mainCommand.equals("chmod")) {  }
+        else if (mainCommand.equals("chmod")) { chmod(argument); }
         else if (mainCommand.equals("clone")) {  }
         else if (mainCommand.equals("mmspt")) {  }
         else if (mainCommand.equals("sign")) {  }
@@ -278,6 +278,25 @@ public class OpenTTY extends MIDlet implements CommandListener {
     private void forCommand(String argument) { argument = argument.trim(); int firstParenthesis = argument.indexOf('('); int lastParenthesis = argument.indexOf(')'); if (firstParenthesis == -1 || lastParenthesis == -1 || firstParenthesis > lastParenthesis) { return; } String key = getCommand(argument); String file = argument.substring(firstParenthesis + 1, lastParenthesis).trim(); String command = argument.substring(lastParenthesis + 1).trim(); if (key.startsWith("(")) { return; } if (key.startsWith("$")) { key = replace(key, "$", ""); } if (file.startsWith("/")) { file = read(file); } else if (file.equals("nano")) { file = nanoContent; } else { file = loadRMS(file, 1); } String[] lines = split(file, '\n'); for (int i = 0; i < lines.length; i++) { if (lines[i] != null || lines[i].length() == 0) { processCommand("set " + key + "=" + lines[i]); processCommand(command); processCommand("unset " + key); } } }
     private void StringEditor(String command) { command = env(command.trim()); String mainCommand = getCommand(command).toLowerCase(); String argument = getArgument(command); if (mainCommand.equals("-2u")) { nanoContent = nanoContent.toUpperCase(); } else if (mainCommand.equals("-2l")) { nanoContent = nanoContent.toLowerCase(); } else if (mainCommand.equals("-d")) { nanoContent = replace(nanoContent, split(argument, ' ')[0], ""); } else if (mainCommand.equals("-a")) { nanoContent = nanoContent.equals("") ? argument : nanoContent + "\n" + argument; } else if (mainCommand.equals("-r")) { nanoContent = replace(nanoContent, split(argument, ' ')[0], split(argument, ' ')[1]); } else if (mainCommand.equals("-l")) { int i = 0; try { i = Integer.parseInt(argument); } catch (NumberFormatException e) { echoCommand(e.getMessage()); return; } echoCommand(split(nanoContent, '\n')[i]); } else if (mainCommand.equals("-s")) { int i = 0; try { i = Integer.parseInt(getCommand(argument)); } catch (NumberFormatException e) { echoCommand(e.getMessage()); return; } Vector lines = new Vector(); String div = getArgument(argument); int start = 0, index; while ((index = nanoContent.indexOf(div, start)) != -1) { lines.addElement(nanoContent.substring(start, index)); start = index + div.length(); } if (start < nanoContent.length()) { lines.addElement(nanoContent.substring(start)); } String[] result = new String[lines.size()]; lines.copyInto(result); if (i >= 0 && i < result.length) { echoCommand(result[i]); } else { echoCommand("null"); } } else if (mainCommand.equals("-p")) { String[] contentLines = split(nanoContent, '\n'); StringBuffer updatedContent = new StringBuffer(); for (int i = 0; i < contentLines.length; i++) { updatedContent.append(argument).append(contentLines[i]).append("\n"); } nanoContent = updatedContent.toString().trim(); } else if (mainCommand.equals("-v")) { String[] lines = split(nanoContent, '\n'); StringBuffer reversed = new StringBuffer(); for (int i = lines.length - 1; i >= 0; i--) { reversed.append(lines[i]).append("\n"); } nanoContent = reversed.toString().trim(); } }
     
+    private void chmod(String node) {
+        if (node == null || node.length() == 0) { return; }
+
+        int status;
+
+        try {
+            if (node.equals("http")) { ((HttpConnection) Connector.open("http://ipinfo.io")).close(); status = 1; node = "javax.microedition.io.Connector.http"; } 
+            else if (node.equals("socket")) { ((SocketConnection) Connector.open("socket://1.1.1.1:53")).close(); status = 1; node = "javax.microedition.io.Connector.socket"; } 
+            else if (node.equals("file")) { FileSystemRegistry.listRoots(); status = 1; node = "javax.microedition.io.Connector.file"; } 
+            
+            else { echoCommand("chmod: " + node + ": not found"); }
+        } 
+        catch (SecurityException e) { status = 2; } 
+        catch (Exception e) { echoCommand(e.getMessage()); }
+    
+        if (status == 1) MIDletLogs("add info Permission '" + node + "' granted");
+        else if (status == 2) MIDletLogs("add error Permission '" + node + "' denied");
+    }
+
 
     // Trace API 
     private void start(String app) { if (app == null || app.length() == 0) { return; } trace.put(app, String.valueOf(1000 + random.nextInt(9000))); }
