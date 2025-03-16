1393d1d03a6110cca4646a7640a33064c1cbe8c6
Mr Lima <felipebr4095@gmail.com>
2025-03-16 15:28:36 -0300

    Update script

---
+++ assets/scripts/update_jbuntu.sh
@@ -0,0 +1,17 @@
+#!/java/bin/sh
+
+install nano
+proxy raw.githubusercontent.com/mrlima4095/OpenTTY-J2ME/main/assets/lib/jbuntu
+install jbuntu
+
+touch
+
+add screen.title=JBuntu Updater
+add screen.content=JBuntu have been updated! Restart MIDlet (Recommended)
+add screen.back=execute @reload; log add info JBuntu have been updated in current session
+add screen.back.label=Reload
+add screen.button=Close MIDlet
+add screen.button.cmd=execute install nano; touch; get initd; add import jbuntu; add jbuntu; add warn JBuntu updated!; add cp initd-backup initd; rm initd-backup
+
+x11 make nano
+get nano
