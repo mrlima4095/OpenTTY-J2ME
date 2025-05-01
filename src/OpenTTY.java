251e8a278970495fa495aff6fc832cc4aa30350b
Mr Lima <felipebr4095@gmail.com>
2025-05-01 16:55:46 -0300

    DD

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -236,11 +236,15 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("fdisk")) { processCommand("lsblk -p"); }
         else if (mainCommand.equals("lsblk")) { boolean hascard = false; StringBuffer roots = new StringBuffer(); Enumeration storage = FileSystemRegistry.listRoots(); while (storage.hasMoreElements()) { String root = (String) storage.nextElement(); roots.append(root).append("\n"); if (root.toLowerCase().indexOf("card") != -1 || root.toLowerCase().indexOf("sd") != -1 || root.toLowerCase().indexOf("e:") != -1 || root.toLowerCase().indexOf("d:") != -1) { hascard = true; } } if (argument.equals("")) { echoCommand("OpenTTY\n| MIDlet\n| RMS\nDevice\n| Storage" + (hascard == true ? "\n| SD Card" : "")); } else if (argument.equals("-x")) { echoCommand("MIDlet;RMS;Storage;" + (hascard == true ? "SD Card;" : "")); } else if (argument.equals("-p")) { echoCommand(roots.toString()); } else { echoCommand("lsblk: " + argument + ": not found"); } }
         else if (mainCommand.equals("dd")) { 
-            if (argument.equals("") || split(argument, ' ').length < 2) { } 
+            if (argument.equals("")) { } 
             else { 
                 try { 
-                    String[] args = split(argument, ' '); 
-
+                    Hashtable args = parseProperties(argument);
+                    String in = (String) args.get("if"), out = (String) args.get("of");
+                    
+                    if (in == null && out == null) { return; }
+                    if (in.startsWith)
+                    
                     FileConnection fileConn = (FileConnection) Connector.open("file:///" + args[0], Connector.READ_WRITE); 
                     if (!fileConn.exists()) fileConn.create(); 
 
