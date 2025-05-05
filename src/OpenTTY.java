73a65e01a5af15818a153ddcbc79d877c5831a47
Lima <felipebr4095@gmail.com>
2025-05-05 13:04:17 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -433,7 +433,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (method.equals("!root")) { Enumeration roots = FileSystemRegistry.listRoots(); while (roots.hasMoreElements()) { if (((String) roots.nextElement()).equals(expression)) { return; } } processCommand(command); }
         else if (method.equals("!thread")) { if (replace(replace(Thread.currentThread().getName(), "MIDletEventQueue", "MIDlet"), "Thread-1", "MIDlet").equals(expression)) { } else { processCommand(command); } }
         else if (method.equals("!trace")) { if (trace.containsKey(expression)) { } else { processCommand(command); } }
-        else if (method.equals("!screen")) { if (desktops.containsKey(expression)) { }else { processCommand(command); } }
+        else if (method.equals("!screen")) { if (desktops.containsKey(expression)) { } else { processCommand(command); } }
         
     }   
     
