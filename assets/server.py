780c56d1a7283f7c66ab59a01eaf08df0eb08506
Mr Lima <felipebr4095@gmail.com>
2025-05-17 13:22:55 -0300

    Server - Not shell

--- assets/server.py
+++ assets/server.py
@@ -21,32 +21,20 @@ import http.client
 import os
 
 class Server:
-    def __init__(self, host='0.0.0.0', port=31522, blacklist_file=None):
+    def __init__(self, host='0.0.0.0', port=31522):
         self.host = host
         self.port = port
-        self.blacklist_file = blacklist_file
-
-    def blacklist(self, file):
-        if file and os.path.isfile(file):
-            with open(file, "rt") as f:
-                return set(f.read().splitlines())
-        return set()
 
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
             server_socket.bind((self.host, self.port))
             server_socket.listen(31522)
-            print(f"Listening on port {self.port}")
+            print(f"[+] Listening on port {self.port}")
 
             while True:
                 client_socket, addr = server_socket.accept()
-                if addr[0] in self.blacklist(self.blacklist_file):
-                    print(f"[-] {addr[0]} is blocked")
-                    client_socket.sendall("You are on the server blacklist!".encode('utf-8'))
-                    client_socket.close()
-                else:
-                    client_thread = threading.Thread(target=self.handle_client, args=(client_socket, addr))
-                    client_thread.start()
+                client_thread = threading.Thread(target=self.handle_client, args=(client_socket, addr))
+                client_thread.start()
 
     def handle_client(self, client_socket, addr):
         print(f"[+] {addr[0]} connected")
@@ -76,30 +64,32 @@ class Server:
         elif cmd == "http": return self.fetch_url(parts[1] if len(parts) > 1 else "")
         elif cmd == "post": return self.post_request(parts[1] if len(parts) > 1 else "")
         elif cmd == "fetch": return self.fetch_repo()
-        else: return self.execute_command(command)
+        else: return "Invalid API request"
 
     def get_file_content(self, filename):
         if not filename:
-            return "Filename is missing."
+            return "Missing filename"
         elif not os.path.isfile(filename):
             return f"File '{filename}' not found."
+        elif filename.startswith("/") or filename.startswith(".."):
+            return "Permission Error"
         else:
             try:
                 with open(filename, "rt") as f:
                     return f.read()
             except Exception as e:
-                return f"Error opening file: {e}"
+                return e
 
     def fetch_url(self, url):
         if not url:
-            return "URL is missing."
+            return "Missing"
         try:
             with urllib.request.urlopen(url if url.startswith("http://") or url.startswith("https://") else "http://" + url) as response:
                 return response.read().decode('utf-8')
         except Exception as e:
-            return f"Error accessing URL: {e}"
+            return e
 
-    def fetch_repo(self): system("git pull")
+    def fetch_repo(self): system("git pull"); return "Server updated!"
 
     def post_request(self, post_command):
         try:
@@ -124,13 +114,7 @@ class Server:
             return response_data
 
         except Exception as e:
-            return f"[-] {e}"
-
-    def execute_command(self, command):
-        try: return subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT).decode('utf-8')
-        except subprocess.CalledProcessError as e: return e.output.decode('utf-8')
+            return e
 
 if __name__ == '__main__':
-    blacklist_file = sys.argv[1] if len(sys.argv) > 1 else None
-    server = Server(blacklist_file=blacklist_file)
-    server.start()
+    Server().start()
