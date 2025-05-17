f9282db2befe90525402d444fc7d5754d4a17904
Mr Lima <felipebr4095@gmail.com>
2025-05-16 22:07:56 -0300

    server

--- assets/server.py
+++ assets/server.py
@@ -75,7 +75,7 @@ class Server:
         if cmd == "get": return self.get_file_content(parts[1] if len(parts) > 1 else "")
         elif cmd == "http": return self.fetch_url(parts[1] if len(parts) > 1 else "")
         elif cmd == "post": return self.post_request(parts[1] if len(parts) > 1 else "")
-        else cmd == "fetch": return self.fetch_repo()
+        elif cmd == "fetch": return self.fetch_repo()
         #else: return self.execute_command(command)
 
     def get_file_content(self, filename):
