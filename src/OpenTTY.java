488ef4d7cc98fa9a473c6b9584365c93aae12c2c
Lima <felipebr4095@gmail.com>
2025-06-06 08:41:07 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -297,7 +297,10 @@ public class OpenTTY extends MIDlet implements CommandListener {
                 } else {
                     StringBuffer sb = new StringBuffer();
                     for (int i = 0; i < results.size(); i++) {
-                        sb.append(results.elementAt(i)).append(base.equals("/home/") ? "\n" : "\t");
+                        if(!results.elementAt(i).equals("/")) {
+                            sb.append(results.elementAt(i)).append(base.equals("/home/") ? "\n" : "\t");
+                        }
+                        
                     }
                     echoCommand(sb.toString().trim());
                 }
