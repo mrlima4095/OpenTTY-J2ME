05b971af1530fa8ea89dc2405613260b7112b05f
Mr Lima <felipebr4095@gmail.com>
2025-06-08 12:49:37 -0300

    Modified command 'THROW'

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -168,7 +168,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         // API 009 - (Threads)
         // |
         // MIDlet Tracker
-        else if (mainCommand.equals("throw")) { Thread.currentThread().interrupt(); if (argument.equals("")) { } else { MIDletLogs("add warn " + argument); } }
+        else if (mainCommand.equals("throw")) { if (argument.equals("")) { } else { MIDletLogs("add warn " + argument); } Thread.currentThread().interrupt(); }
         else if (mainCommand.equals("mmspt")) { echoCommand(replace(replace(Thread.currentThread().getName(), "MIDletEventQueue", "MIDlet"), "Thread-1", "MIDlet")); }
         else if (mainCommand.equals("bg")) { final String bgCommand = argument; new Thread(new Runnable() { public void run() { processCommand(bgCommand); } }, "Background").start(); }
 
