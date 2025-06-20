11240d240a841f637452317e3b6fdaa1eed44fdd
Mr Lima <felipebr4095@gmail.com>
2025-06-20 16:00:13 -0300

    latest

--- assets/scripts/latest.sh
+++ assets/scripts/latest.sh
@@ -5,5 +5,5 @@ set ONLINE_VERSION=1.14.4
 
 
 if ($VERSION == $ONLINE_VERSION) echo already up to date.
-if ($VERSION != $ONLINE_VERSION) execute echo A new version was released!; echo; echo Local - OpenTTY $VERSION; echo Server - OpenTTY $ONLINE_VERSION; open https://github.com/mrlima4095/OpenTTY-J2ME/releases;
+if ($VERSION != $ONLINE_VERSION) execute echo A new version was released!; echo; echo Local - OpenTTY $VERSION; echo Server - OpenTTY $ONLINE_VERSION; open http://opentty.xyz/repo/versions/OpenTTY-$ONLINE_VERSION.jar;
 
