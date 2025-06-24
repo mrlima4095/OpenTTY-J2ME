cf1b64e8ce3239d11db24075dffcf0e22caaa425
Lima <felipebr4095@gmail.com>
2025-06-24 10:38:45 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -281,19 +281,6 @@ public class OpenTTY extends MIDlet implements CommandListener {
 
         //else if (mainCommand.equals("")) {  }
         //else if (mainCommand.equals("")) {  }
-        else if (mainCommand.equals("function")) {
-            if (argument.equals("")) { } 
-            else {
-                int braceIndex = argument.indexOf('{');
-                int braceEnd = argument.lastIndexOf('}');
-                if (braceIndex != -1 && braceEnd != -1 && braceEnd > braceIndex) {
-                    String name = getCommand(argument).trim();
-                    String body = replace(argument.substring(braceIndex + 1, braceEnd).trim(), ";", "\n");
-                    functions.put(name, body);
-                } 
-                else { echoCommand("invalid syntax"); }
-            }
-        }
 
         // API 014 - (OpenTTY)
         // |
@@ -309,7 +296,8 @@ public class OpenTTY extends MIDlet implements CommandListener {
         // OpenTTY Packages
         else if (mainCommand.equals("about")) { about(argument); }
         else if (mainCommand.equals("import")) { importScript(argument); }
-        else if (mainCommand.equals("run")) { processCommand(". " + argument, false); }
+        else if (mainCommand.equals("run")) { processCommand(". " + argument, false); 
+        else if (mainCommand.equals("function")) { if (argument.equals("")) { } else { int braceIndex = argument.indexOf('{'), braceEnd = argument.lastIndexOf('}'); if (braceIndex != -1 && braceEnd != -1 && braceEnd > braceIndex) { String name = getCommand(argument).trim(); String body = replace(argument.substring(braceIndex + 1, braceEnd).trim(), ";", "\n"); functions.put(name, body); } else { echoCommand("invalid syntax"); } } }
 
         else if (mainCommand.equals("!")) { echoCommand(env("main/$RELEASE"));  }
         else if (mainCommand.equals("!!")) { if (history.size() > 0) { stdin.setString((String) history.elementAt(history.size() - 1)); } }
