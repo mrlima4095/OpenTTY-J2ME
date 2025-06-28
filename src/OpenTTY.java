fd8a2712a68a15f0041b5560d8255840399a434f
Mr Lima <felipebr4095@gmail.com>
2025-06-28 14:23:39 -0300

    Build "2025-1.15-02x09" - Return codes

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -19,7 +19,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
                       paths = new Hashtable(), desktops = new Hashtable(), trace = new Hashtable();
     private Vector stack = new Vector(), history = new Vector(), sessions = new Vector();
     private String username = loadRMS("OpenRMS"), nanoContent = loadRMS("nano");
-    private String logs = "", path = "/home/", build = "2025-1.15-02x08"; 
+    private String logs = "", path = "/home/", build = "2025-1.15-02x09"; 
     private Display display = Display.getDisplay(this);
     private Form form = new Form("OpenTTY " + getAppProperty("MIDlet-Version"));
     private TextField stdin = new TextField("Command", "", 256, TextField.ANY);
