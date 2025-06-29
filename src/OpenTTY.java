ca13accfe2da4b13c2c41b4e887a9b2bff6750fb
Mr Lima <felipebr4095@gmail.com>
2025-06-28 22:59:04 -0300

    cd - Code wrap

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -212,8 +212,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("pwd")) { echoCommand(path); }
         else if (mainCommand.equals("umount")) { paths = new Hashtable(); }
         else if (mainCommand.equals("mount")) { if (argument.equals("")) { } else { mount(getcontent(argument)); } }
-        else if (mainCommand.equals("cd")) { 
-            if (argument.equals("")) { path = "/home/"; } 
+        else if (mainCommand.equals("cd")) { if (argument.equals("")) { path = "/home/"; } 
             else if (argument.equals("..")) { if (path.equals("/")) { return 0; } int lastSlashIndex = path.lastIndexOf('/', path.endsWith("/") ? path.length() - 2 : path.length() - 1); path = (lastSlashIndex <= 0) ? "/" : path.substring(0, lastSlashIndex + 1); } 
             else { 
                 String TARGET = argument.startsWith("/") ? argument : (path.endsWith("/") ? path + argument : path + "/" + argument); 
