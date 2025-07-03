a2e8a1a1c59b77b7d3b4808e201fd01d4f8f3f94
Lima <felipebr4095@gmail.com>
2025-07-02 23:07:53 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -764,7 +764,6 @@ public class OpenTTY extends MIDlet implements CommandListener {
             shell.put(command, TABLE);
         }
 
-
         return 0;
     }
     private int runScript(String script) { String[] CMDS = split(script, '\n'); for (int i = 0; i < CMDS.length; i++) { int STATUS = processCommand(CMDS[i].trim()); if (STATUS != 0) { return STATUS; } } return 0; }
