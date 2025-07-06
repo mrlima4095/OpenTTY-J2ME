f15613d0f5eb620e889d681220741034546da799
Mr Lima <felipebr4095@gmail.com>
2025-07-06 15:24:55 -0300

    New Java Runtime version - by JVM name

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -618,72 +618,28 @@ public class OpenTTY extends MIDlet implements CommandListener {
 
         if (mainCommand.equals("")) { viewer("Java ME", env("Java 1.2 (OpenTTY Edition)\n\nMicroEdition-Config: $CONFIG\nMicroEdition-Profile: $PROFILE")); }
         else if (mainCommand.equals("-class")) { if (argument.equals("")) { } else { int STATUS = javaClass(argument); echoCommand(STATUS == 0 ? "true" : "false"); return STATUS; } } 
-        else if (mainCommand.equals("--name")) {
+        else if (mainCommand.equals("--version")) {
             String s;
             StringBuffer sb = new StringBuffer();
 
             if ((s = System.getProperty("java.vm.name")) != null) {
                 sb.append(s).append(", ").append(System.getProperty("java.vm.vendor"));
-                if ((s = System.getProperty("java.vm.version")) != null) {
-                    sb.append('\n').append(s);
-                }
-                if ((s = System.getProperty("java.vm.specification.name")) != null) {
-                    sb.append('\n').append(s);
-                }
-            } else if ((s = System.getProperty("com.ibm.oti.configuration")) != null) {
+                if ((s = System.getProperty("java.vm.version")) != null) { sb.append('\n').append(s); }
+                if ((s = System.getProperty("java.vm.specification.name")) != null) { sb.append('\n').append(s); }
+            } 
+            else if ((s = System.getProperty("com.ibm.oti.configuration")) != null) { 
                 sb.append("J9 VM, IBM (").append(s).append(')');
-                if ((s = System.getProperty("java.fullversion")) != null) {
-                    sb.append("\n\n").append(s);
-                }
-            } else if ((s = System.getProperty("java.fullversion")) != null) {
-                sb.append(s);
-            } else if ((s = System.getProperty("com.oracle.jwc.version")) != null) {
-                sb.append("OJWC v").append(s).append(", Oracle");
-            } else if (javaClass(new String[] {
-                "com.sun.cldchi.io.ConsoleOutputStream",
-                "com.sun.cldchi.jvm.JVM"
-            })) {
-                sb.append("CLDC Hotspot Implementation, Sun");
-            } else if (javaClass(new String[] {
-                "com.sun.midp.io.InternalConnector",
-                "com.sun.midp.io.ConnectionBaseAdapter",
-                "com.sun.midp.Main"
-            })) {
-                sb.append("KVM, Sun (MIDP)");
-            } else if (javaClass(new String[] {
-                "com.sun.cldc.util.j2me.CalendarImpl",
-                "com.sun.cldc.i18n.Helper",
-                "com.sun.cldc.io.ConsoleOutputStream",
-                "com.sun.cldc.i18n.uclc.DefaultCaseConverter"
-            })) {
-                sb.append("KVM, Sun (CLDC)");
-            } else if (javaClass(new String[] {
-                "com.jblend.util.SortedVector",
-                "com.jblend.tck.socket2http.Protocol",
-                "com.jblend.io.j2me.resource.Protocol",
-                "com.jblend.security.midp20.SecurityManagerImpl",
-                "com.jblend.security.midp20.UserConfirmDialogImpl",
-                "jp.co.aplix.cldc.io.MIDPURLChecker",
-                "jp.co.aplix.cldc.io.j2me.http.HttpConnectionImpl"
-            })) {
-                sb.append("JBlend, Aplix");
-            } else if (javaClass(new String[] {
-                "com.jbed.io.CharConvUTF8",
-                "com.jbed.runtime.MemSupport",
-                "com.jbed.midp.lcdui.GameCanvasPeer",
-                "com.jbed.microedition.media.CoreManager",
-                "com.jbed.runtime.Mem",
-                "com.jbed.midp.lcdui.GameCanvas",
-                "com.jbed.microedition.media.Core"
-            })) {
-                sb.append("Jbed, Esmertec/Myriad Group");
-            } else if (javaClass(new String[] {
-                "MahoTrans.IJavaObject"
-            })) {
-                sb.append("MahoTrans");
-            } else {
-                sb.append("Unknown");
-            }
+                if ((s = System.getProperty("java.fullversion")) != null) { sb.append("\n\n").append(s); }
+            } 
+            else if ((s = System.getProperty("java.fullversion")) != null) { sb.append(s); } 
+            else if ((s = System.getProperty("com.oracle.jwc.version")) != null) { sb.append("OJWC v").append(s).append(", Oracle"); } 
+            else if (javaClass(new String[] { "com.sun.cldchi.io.ConsoleOutputStream", "com.sun.cldchi.jvm.JVM" })) { sb.append("CLDC Hotspot Implementation, Sun"); } 
+            else if (javaClass(new String[] { "com.sun.midp.io.InternalConnector", "com.sun.midp.io.ConnectionBaseAdapter", "com.sun.midp.Main" })) { sb.append("KVM, Sun (MIDP)"); } 
+            else if (javaClass(new String[] { "com.sun.cldc.util.j2me.CalendarImpl", "com.sun.cldc.i18n.Helper", "com.sun.cldc.io.ConsoleOutputStream", "com.sun.cldc.i18n.uclc.DefaultCaseConverter" })) { sb.append("KVM, Sun (CLDC)"); } 
+            else if (javaClass(new String[] { "com.jblend.util.SortedVector", "com.jblend.tck.socket2http.Protocol", "com.jblend.io.j2me.resource.Protocol", "com.jblend.security.midp20.SecurityManagerImpl", "com.jblend.security.midp20.UserConfirmDialogImpl", "jp.co.aplix.cldc.io.MIDPURLChecker", "jp.co.aplix.cldc.io.j2me.http.HttpConnectionImpl" })) { sb.append("JBlend, Aplix"); } 
+            else if (javaClass(new String[] { "com.jbed.io.CharConvUTF8", "com.jbed.runtime.MemSupport", "com.jbed.midp.lcdui.GameCanvasPeer", "com.jbed.microedition.media.CoreManager", "com.jbed.runtime.Mem", "com.jbed.midp.lcdui.GameCanvas", "com.jbed.microedition.media.Core" })) { sb.append("Jbed, Esmertec/Myriad Group"); } 
+            else if (javaClass(new String[] { "MahoTrans.IJavaObject" })) { sb.append("MahoTrans"); }  
+            else { sb.append("Unknown"); }
 
             if ((s = System.getProperty("java.version")) != null) {
                 sb.append("\nJava ").append(s);
@@ -692,7 +648,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
             }
 
             s = System.getProperty("os.name");
-            if (s != null) {
+            if (s != null) { 
                 sb.append('\n').append(s);
                 s = System.getProperty("os.version");
                 if (s != null) sb.append(' ').append(s);
@@ -702,7 +658,6 @@ public class OpenTTY extends MIDlet implements CommandListener {
 
             echoCommand(sb.append('\n').toString());
         }
-        else if (mainCommand.equals("--version")) { echoCommand("Java 1.2 (OpenTTY Edition)"); }
         else { 
             String code = getcontent(mainCommand); 
             Hashtable objects = new Hashtable(); 
