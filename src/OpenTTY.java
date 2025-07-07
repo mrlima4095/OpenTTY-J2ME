3ea6e424c9a3a0686c358dcb5dd1a707077c26b0
Mr Lima <felipebr4095@gmail.com>
2025-07-07 18:44:40 -0300

    -1+1

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -752,8 +752,8 @@ public class OpenTTY extends MIDlet implements CommandListener {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
         out.write(new byte[] {
-            (byte) 0xCA
-            //(byte) 0xFE,
+            //(byte) 0xCA
+            (byte) 0xFE
             //(byte) 0xBA,
             //(byte) 0xBE
         });
