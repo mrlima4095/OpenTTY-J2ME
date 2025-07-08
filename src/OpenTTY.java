b04de8995a8ce9223ca448224e398bcc6d52b5e8
Mr Lima <felipebr4095@gmail.com>
2025-07-07 21:55:50 -0300

    NM

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -781,6 +781,61 @@ public class OpenTTY extends MIDlet implements CommandListener {
 
     return out.toByteArray();
 }
+    private byte[] mnemonicsToBytes(String mnemonics) throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        Hashtable opcodes = new Hashtable();
+        opcodes.put("nop", new Integer(0x00));
+        opcodes.put("aconst_null", new Integer(0x01));
+        opcodes.put("iconst_0", new Integer(0x03));
+        opcodes.put("iconst_1", new Integer(0x04));
+        opcodes.put("iconst_2", new Integer(0x05));
+        opcodes.put("iload_0", new Integer(0x1A));
+        opcodes.put("aload_0", new Integer(0x2A));
+        opcodes.put("istore_0", new Integer(0x3B));
+        opcodes.put("astore_0", new Integer(0x4B));
+        opcodes.put("pop", new Integer(0x57));
+        opcodes.put("iadd", new Integer(0x60));
+        opcodes.put("return", new Integer(0xB1));
+        opcodes.put("invokespecial", new Integer(0xB7));
+        // ...adicione outras conforme precisar
+
+        // Quebrar linhas simples, sem regex
+        int start = 0;
+        int length = mnemonics.length();
+        while (start < length) {
+            int end = mnemonics.indexOf('\n', start);
+            if (end == -1) end = length;
+            String line = mnemonics.substring(start, end).trim();
+            start = end + 1;
+
+            if (line.length() == 0) continue;
+
+            // Quebrar por espaço simples
+            int spaceIndex = line.indexOf(' ');
+            String instr = line;
+            String arg = null;
+            if (spaceIndex != -1) {
+                instr = line.substring(0, spaceIndex);
+                arg = line.substring(spaceIndex + 1).trim();
+            }
+
+            Integer opcodeInt = (Integer) opcodes.get(instr);
+            if (opcodeInt == null) throw new Exception("Opcode desconhecido: " + instr);
+            out.write(opcodeInt.intValue());
+
+            if (arg != null) {
+                String[] args = split(arg, ' ');
+                for (int i = 0; i < args.length; i++) {
+                    int val = Integer.parseInt(args[i]);
+                    out.write(val & 0xFF);
+                }
+            }
+
+        }
+
+        return out.toByteArray();
+    }
 private String extractClassName(String code) {
     int idx = code.indexOf("class ");
     if (idx == -1) return "Unnamed";
