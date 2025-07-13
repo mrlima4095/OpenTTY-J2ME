b2602caff69fa52e15bb4a2677394036879f5ce6
Lima <felipebr4095@gmail.com>
2025-07-13 18:21:28 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -282,10 +282,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         //else if (mainCommand.equals("")) {  }
         else if (mainCommand.equals("sudo")) { if (argument.equals("")) { } else { new Credentials(argument); } }
         else if (mainCommand.equals("passwd")) { if (root) { if (argument.equals("")) { } else { writeRMS("/home/.passwd", "" + argument.hashCode()); } } else { echoCommand("passwd: permission denied"); return 13; } }
-        else if (mainCommand.equals("which")) {
-            if (argument.equals("")) { }
-            else { echoCommand(shell.containsKey(argument) ? "shell" : (aliases.containsKey(argument) ? "alias" : (functions.containsKey(argument) ? "function" : ""))); }
-        }
+        else if (mainCommand.equals("which")) { if (argument.equals("")) { } else { echoCommand(shell.containsKey(argument) ? "shell" : (aliases.containsKey(argument) ? "alias" : (functions.containsKey(argument) ? "function" : ""))); } }
 
         // API 014 - (OpenTTY)
         // |
