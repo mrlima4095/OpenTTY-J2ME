c71b076a8bec4f917a6a6bc59e1569ffaf687160
Lima <felipebr4095@gmail.com>
2025-07-13 16:29:58 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -281,7 +281,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
 
         //else if (mainCommand.equals("")) {  }
         //else if (mainCommand.equals("")) {  }
-        else if (mainCommand.equals("passwd")) { processCommand("execute import /java/lib/jauth2; sudo passwd;", true); }
+        else if (mainCommand.equals("passwd")) {  }
         else if (mainCommand.equals("which")) {
             if (argument.equals("")) { }
             else { echoCommand(shell.containsKey(argument) ? "shell" : (aliases.containsKey(argument) ? "alias" : (functions.containsKey(argument) ? "function" : ""))); }
