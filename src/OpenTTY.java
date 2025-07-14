1f26b7dc406290a077f835d945f9fe32841f87cf
Lima <felipebr4095@gmail.com>
2025-07-13 23:15:56 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -98,7 +98,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
                 username = argument;
             } 
         }
-        else if (mainCommand.equals("sudo")) { if (argument.equals("")) { } else { new Credentials(argument); } }
+        else if (mainCommand.equals("sudo")) { if (argument.equals("")) { } else if (root) { return processCommand(argument, ignore, root); } else { new Credentials(argument); } }
         else if (mainCommand.equals("whoami") || mainCommand.equals("logname")) { echoCommand(root == true ? "root": username); }
         else if (mainCommand.equals("sh") || mainCommand.equals("login")) { return processCommand("import /java/bin/sh", false); }
         else if (mainCommand.equals("logout")) {
