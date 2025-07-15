5b87dff74d79423aa3a6452436b46de3b3f5cd47
Lima <felipebr4095@gmail.com>
2025-07-14 23:41:27 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -492,8 +492,8 @@ public class OpenTTY extends MIDlet implements CommandListener {
 
         return 0; 
     }
-    private int writeRMS(String filename, byte[] data) { return writeRMS(filename, data, username.equals("root") ? true : false); }
-    private int writeRMS(String filename, String data) { return writeRMS(filename, data.getBytes()); }
+    private int writeRMS(String filename, byte[] data) { return writeRMS(filename, data, false); }
+    private int writeRMS(String filename, String data) { return writeRMS(filename, data.getBytes(), false); }
     private String loadRMS(String filename) { return read("/home/" + filename); }
     // |
     // Text Manager
