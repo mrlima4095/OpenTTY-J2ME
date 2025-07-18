5665452b9a57fdb691979cd2eeeac4c800f34118
Lima <felipebr4095@gmail.com>
2025-07-17 23:27:23 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -293,7 +293,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("@exec")) { commandAction(EXECUTE, display.getCurrent()); }
         else if (mainCommand.equals("@alert")) { try { display.vibrate(argument.equals("") ? 500 : Integer.parseInt(argument) * 100); } catch (Exception e) { echoCommand(getCatch(e)); return 2; } }
         else if (mainCommand.equals("@reload")) { shell = new Hashtable(); aliases = new Hashtable(); username = loadRMS("OpenRMS"); MIDletLogs("add debug API reloaded"); processCommand("execute x11 stop; x11 init; x11 term; run initd; sh;"); }
-        else if (mainCommand.startsWith("@")) {  display.vibrate(500); }
+        else if (mainCommand.startsWith("@")) { display.vibrate(500); }
 
         // API 015 - (Scripts)
         // |
