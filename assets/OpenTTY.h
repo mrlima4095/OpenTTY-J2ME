69216fa399add99d581a402bb888d79b3757c6d7
Mr Lima <felipebr4095@gmail.com>
2025-07-22 19:46:29 -0300

    C2ME APi

--- assets/OpenTTY.h
+++ assets/OpenTTY.h
@@ -11,7 +11,14 @@ char getuser() { return "$USERNAME"; }
 
 // Text API 
 char append(char text, char buffer) { return "%buffer %text"; }
+char trim(char text) {  }
 int length(char text) {  }
+
+
+int call(int phone) { return exec("call %phone"); }
+int exec(char cmd) { exec("%cmd"); return getenv("OUTPUT"); }
+
+char getenv(char key) {  }
 
 int main() {
 	printf("OpenTTY C2ME API");
