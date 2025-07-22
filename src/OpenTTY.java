fae0dd7b1e9666e89a8d9e59e1e19634a7c8ade0
Mr Lima <felipebr4095@gmail.com>
2025-07-22 14:15:42 -0300

    thown

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -619,13 +619,13 @@ public class OpenTTY extends MIDlet implements CommandListener {
         for (Enumeration e = vars.keys(); e.hasMoreElements(); ) {
             String name = (String) e.nextElement(), value = (String) ((Hashtable) vars.get(name)).get("value");
 
-            value = value == null || value.length() == 0 ? "" : /*value.equals("' '") ? " " :*/ value;
+            value = value == null || value.length() == 0 ? "" : value;
 
             if (expr.startsWith("\"") && expr.endsWith("\"") || expr.startsWith("'") && expr.endsWith("'")) { expr = replace(expr, "%" + name, value); } 
             else { expr = replaceVarOnly(expr, name, value); }
         }
 
-        if (expr.startsWith("\"") && expr.endsWith("\"") || expr.startsWith("'") && expr.endsWith("'")) { return expr.substring(1, expr.length() - 1); }
+        if (expr.startsWith("\"") && expr.endsWith("\"") || expr.startsWith("'") && expr.endsWith("'") && !expr.equals("' '")) { return expr.substring(1, expr.length() - 1); }
 
         while (true) {
             int open = expr.indexOf('(');
