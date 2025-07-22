a0a19a27f444075f9f5b99e408515a334db91283
Lima <felipebr4095@gmail.com>
2025-07-21 22:53:22 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -619,7 +619,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         for (Enumeration e = vars.keys(); e.hasMoreElements(); ) {
             String name = (String) e.nextElement(), value = (String) ((Hashtable) vars.get(name)).get("value");
 
-            if (value.equals("' '")) { value = " "; }
+            //if (value.equals("' '")) { value = ""; }
 
             if (expr.startsWith("\"") && expr.endsWith("\"")) { expr = replace(expr, "%" + name, value); } 
             else { expr = replaceVarOnly(expr, name, value); }
