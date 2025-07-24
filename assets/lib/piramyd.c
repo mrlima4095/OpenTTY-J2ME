2d459cc35c6baf2ff23d618a1199ba18244bc9a2
Mr Lima <felipebr4095@gmail.com>
2025-07-24 19:40:55 -0300

    piramyd.c

---
+++ assets/lib/piramyd.c
@@ -0,0 +1,42 @@
+char append(char text, char buffer) { return "%buffer%text"; }
+
+int main() {
+    int i = 0, j, n = 6;
+    char line;
+
+    j = 0; line = "";
+    while (j < (2 * n)) {
+        line = append("*", line);
+        j = j + 1;
+    }
+    printf(line);
+
+    while (i < n) {
+        line = "*"; 
+        j = 0;
+        while (j < n - i - 1) {
+            line = append(" ", line);
+            j = j + 1;
+        }
+        j = 0;
+        while (j < (2 * i + 1)) {
+            line = append("*", line);
+            j = j + 1;
+        }
+        j = 0;
+        while (j < n - i - 1) {
+            line = append(" ", line);
+            j = j + 1;
+        }
+        line = append("*", line);
+        printf(line);
+        i = i + 1;
+    }
+
+    j = 0; line = "";
+    while (j < (2 * n)) {
+        line = append("*", line);
+        j = j + 1;
+    }
+    printf(line);
+}
