67991d074cf7568ad98c98629a512b231375d2af
Mr Lima <felipebr4095@gmail.com>
2025-07-24 21:59:59 -0300

    seed base v2

--- assets/OpenTTY.h
+++ assets/OpenTTY.h
@@ -16,7 +16,7 @@ char append(char text, char buffer) { return "%buffer %text"; }
 int random(int max, int seed) {
     int i;
     if (seed != 0) { i = seed; } 
-    else { i = 1; };
+    else { i = 4095; };
 
     i = i * 1162025 + 12345;
 
