1ae3f01800e03de7eca4d36d44f1713b15f8dfef
Lima <felipebr4095@gmail.com>
2025-07-25 00:01:50 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -626,7 +626,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
     }
     private String format(String expr) throws RuntimeException {
         if (expr == null || expr.length() == 0) { return "' '"; }
-        if (expr.startsWith("\"") && expr.endsWith("\"") || expr.startsWith("'") && expr.endsWith("'")) { return expr.substring(1, expr.length() - 1); }
+        if (expr.startsWith("\"") && expr.endsWith("\"") || expr.startsWith("'") && expr.endsWith("'")) { return env(expr.substring(1, expr.length() - 1)); }
         
         return env(expr);
     }
