13c213cc996269ef797b0b3e204de690412c1d43
Lima <felipebr4095@gmail.com>
2025-07-25 00:31:16 -0300

    update OpenTTY.h

--- assets/OpenTTY.h
+++ assets/OpenTTY.h
@@ -12,8 +12,9 @@ char getuser() { return "$USERNAME"; }
 // Text API 
 char append(char text, char buffer) { return "%buffer %text"; }
 
-
-
+// MIDlet
+int ttysize() { return len(open("stdout")); }
+int clear() { return exec("clear stdout"); }
 
 int main() {
 	printf("OpenTTY C2ME API");
