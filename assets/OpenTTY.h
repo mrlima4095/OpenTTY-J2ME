6c88daac03ea6c492d9bafb6c9a9058f65e3064c
Lima <felipebr4095@gmail.com>
2025-07-24 22:45:44 -0300

    update OpenTTY.h

--- assets/OpenTTY.h
+++ assets/OpenTTY.h
@@ -13,16 +13,6 @@ char getuser() { return "$USERNAME"; }
 char append(char text, char buffer) { return "%buffer %text"; }
 
 
-char random(int max, int seed) {
-    if (seed == 0) { seed = 4095; };
-
-    seed = seed * 1162025 + 12345;
-
-    while (seed > max) {
-    	seed = seed / 2;
-    };
-    return seed;
-}
 
 
 int main() {
