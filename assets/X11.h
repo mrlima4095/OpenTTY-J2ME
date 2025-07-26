32594fb2d97166b319de21f3ee78b7f53cd60486
Lima <felipebr4095@gmail.com>
2025-07-26 19:04:34 -0300

    update X11.h

--- assets/X11.h
+++ assets/X11.h
@@ -6,21 +6,12 @@
 
 #include "OpenTTY.h"
 
-int WindowTitle(char text) {
-    return exec("x11 title %text");
-}
-int Alert(char msg) {
-    return exec("warn %msg");
-}
+int Title(char title) { return exec("title %title"); }
+int WindowTitle(char title) { return exec("x11 title %text"); }
+int Alert(char msg) { return exec("warn %msg"); }
 
-int SaveWindow(char name) {
-    return exec("x11 set %name");
-}
-int LoadWindow(char name) {
-    return exec("x11 load %name");
-}
+int SaveWindow(char name) { return exec("x11 set %name"); }
+int LoadWindow(char name) { return exec("x11 load %name"); }
 
-int SetTicker(char msg) {
-    return exec("x11 tick %msg");
-}
+int SetTicker(char msg) { return exec("x11 tick %msg"); }
 
