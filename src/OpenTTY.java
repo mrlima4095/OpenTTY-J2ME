770860e4e3fa055fa86eccaced5aace344215d44
Mr Lima <felipebr4095@gmail.com>
2025-07-29 18:05:15 -0300

    Code wrap

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -245,11 +245,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("nc")) { new RemoteConnection(argument); }
         // |
         else if (mainCommand.equals("wrl")) { return wireless(argument); }
-        else if (mainCommand.equals("who")) { 
-            StringBuffer SESSIONS = new StringBuffer(); 
-            for (int i = 0; i < sessions.size(); i++) { SESSIONS.append((String) sessions.elementAt(i)).append("\n"); } 
-            echoCommand(SESSIONS.toString().trim()); 
-        }
+        else if (mainCommand.equals("who")) { StringBuffer SESSIONS = new StringBuffer(); for (int i = 0; i < sessions.size(); i++) { SESSIONS.append((String) sessions.elementAt(i)).append("\n"); } echoCommand(SESSIONS.toString().trim()); }
         // |
         // IP Tools
         else if (mainCommand.equals("fw")) { echoCommand(request("http://ipinfo.io/" + (argument.equals("") ? "json" : argument))); }
