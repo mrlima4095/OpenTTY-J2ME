e3d6a9e219fedc0c1be55f0eedf596fe0a28ed1e
Lima <felipebr4095@gmail.com>
2025-08-03 11:41:08 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -141,10 +141,16 @@ public class OpenTTY extends MIDlet implements CommandListener {
                 
             } else {
                 for (int i = 0; i < args.length; i++) {
-                    if (attributes.containsKey(args[i])) { echoCommand(argument + "=" + (String) attributes.get(args[i])); } 
+                    if (attributes.containsKey(args[i])) {
+                        echoCommand(argument + "=" + (String) attributes.get(args[i])); } 
                     else { echoCommand("env: " + argument + ": not found"); }
                 }
             }
+            else if (attributes.containsKey(argument)) { 
+                echoCommand(argument + "=" + (String) attributes.get(argument)); 
+                
+            } 
+            else { echoCommand("env: " + argument + ": not found"); return 127; } 
             
         }
 
