5a0fcf616ff2ea42d45597f58b6ee776faa23949
Lima <felipebr4095@gmail.com>
2025-08-06 23:40:53 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -189,7 +189,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         // Process
         else if (mainCommand.equals("start") || mainCommand.equals("stop") || mainCommand.equals("kill")) { 
             for (int i = 0; i < args.length; i++) {
-                int STATUS = mainCommand.equals("start") ? start(args[i], null, null, root) : mainCommand.equals("stop") ? stop(args[i], root) : kill(args[i], true, root);
+                int STATUS = mainCommand.equals("start") ? start(args[i], null, root) : mainCommand.equals("stop") ? stop(args[i], root) : kill(args[i], true, root);
             }
         } 
         else if (mainCommand.equals("ps")) {
