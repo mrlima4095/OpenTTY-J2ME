77b423c71a0d113de3ed001c38cf389558dcb0f7
Mr Lima <felipebr4095@gmail.com>
2025-08-09 10:50:00 -0300

    ++ bypáss root

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -537,14 +537,14 @@ public class OpenTTY extends MIDlet implements CommandListener {
         // |
         // X11 Loader
         else if (mainCommand.equals("term")) { return loadScreen(form); }
-        else if (mainCommand.equals("stop")) { form.setTitle(null); form.setTicker(null); form.deleteAll(); xserver("cmd hide"); trace.remove("2"); form.removeCommand(EXECUTE); }
+        else if (mainCommand.equals("stop")) { form.setTitle(null); form.setTicker(null); form.deleteAll(); xserver("cmd hide", root); trace.remove("2"); form.removeCommand(EXECUTE); }
         else if (mainCommand.equals("init")) { 
             form.setTitle(env("OpenTTY $VERSION")); 
             form.append(stdout); form.append(stdin); form.addCommand(EXECUTE); 
             processCommand("execute title; x11 cmd; start x11-server;", false, true);
             form.setCommandListener(this); 
         }
-        else if (mainCommand.equals("xfinit")) { if (argument.equals("")) { xserver("init"); } if (argument.equals("stdin")) { form.append(stdin); } else if (argument.equals("stdout")) { form.append(stdout); } }
+        else if (mainCommand.equals("xfinit")) { if (argument.equals("")) { xserver("init", root); } if (argument.equals("stdin")) { form.append(stdin); } else if (argument.equals("stdout")) { form.append(stdout); } }
         
         else if (mainCommand.equals("cmd")) { Command[] CMDS = { HELP, NANO, CLEAR, HISTORY }; for (int i = 0; i < CMDS.length; i++) { if (argument.equals("hide")) { form.removeCommand(CMDS[i]); } else { form.addCommand(CMDS[i]); } } }
         // | 
