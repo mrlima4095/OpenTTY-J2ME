4970b8f5fdbe76b4c02f5ee69d7701c3cddeaa23
Mr Lima <felipebr4095@gmail.com>
2025-08-09 13:54:54 -0300

    else { ... }

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -960,16 +960,18 @@ public class OpenTTY extends MIDlet implements CommandListener {
     private int start(String app, String pid, String collector, boolean root) {
         if (app == null || app.length() == 0) { return 2; }
 
-        /*if (app.equals("sh") || app.equals("x11-wm")) {
+        if (app.equals("sh") || app.equals("x11-wm")) {
             pid = app.equals("sh") ? "1" : "2";
             collector = app.equals("sh") ? "exit" : "x11 stop";
 
             if (trace.containsKey(pid)) { return 68; }
+            else if (app.equals("x11-wm")) { xserver("x11 init"); }
             else if (app.equals("sh")) { sessions.put(pid, "127.0.0.1"); }
-        }/*/
+        } else {
+            while (trace.containsKey(pid) || pid == null || pid.length() == 0) { pid = genpid(); }
+        }
 
-        if (pid == null || pid.length() == 0) { pid = genpid(); }
-        //if (trace.containsKey(pid)) { return start(app, null, collector, root); }
+        
 
         Hashtable proc = new Hashtable();
         proc.put("name", app); proc.put("owner", root ? "root" : username);
