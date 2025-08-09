b933b6961ca0f3e209c4d72b821c1b5ebc0a2bf0
Mr Lima <felipebr4095@gmail.com>
2025-08-09 14:53:35 -0300

    throw loop parts debugging

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -958,7 +958,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
             collector = app.equals("sh") ? "exit" : "x11 stop";
 
             if (trace.containsKey(pid)) { return 68; }
-            else if (app.equals("x11-wm")) { xserver("x11 init", root); }
+            else if (app.equals("x11-wm")) {  }
             else if (app.equals("sh")) { sessions.put(pid, "127.0.0.1"); }
         } else { while (trace.containsKey(pid) || pid == null || pid.length() == 0) { pid = genpid(); } } 
 
