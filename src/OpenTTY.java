22a46f4ea11d421723a1883e77c79a20e5a244cb
Mr Lima <felipebr4095@gmail.com>
2025-08-12 20:02:58 -0300

    TextBoz nano

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -25,8 +25,8 @@ public class OpenTTY extends MIDlet implements CommandListener {
     private String username = loadRMS("OpenRMS"), nanoContent = loadRMS("nano");
     private String logs = "", path = "/home/", build = "2025-1.16-02x48"; 
     private Display display = Display.getDisplay(this);
-    private TextBox nano = new TextBox("Nano", "", 31522, TextField.ANY);
     private List preview = new List(null, List.IMPLICIT);
+    private TextBox nano = new TextBox("Nano", "", 31522, TextField.ANY);
     private Form form = new Form("OpenTTY " + getAppProperty("MIDlet-Version"));
     private TextField stdin = new TextField("Command", "", 256, TextField.ANY);
     private StringItem stdout = new StringItem("", "Welcome to OpenTTY " + getAppProperty("MIDlet-Version") + "\nCopyright (C) 2025 - Mr. Lima\n"),
