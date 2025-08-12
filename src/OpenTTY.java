eda79724c162a140ccef8541de190653bcc788c2
Mr Lima <felipebr4095@gmail.com>
2025-08-12 18:00:32 -0300

    Code unwrap

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -33,7 +33,11 @@ public class OpenTTY extends MIDlet implements CommandListener {
     private Form form = new Form("OpenTTY " + getAppProperty("MIDlet-Version"));
     private TextField stdin = new TextField("Command", "", 256, TextField.ANY);
     private StringItem stdout = new StringItem("", "Welcome to OpenTTY " + getAppProperty("MIDlet-Version") + "\nCopyright (C) 2025 - Mr. Lima\n");
-    private Command EXECUTE = new Command("Send", Command.OK, 0), HELP = new Command("Help", Command.SCREEN, 1), NANO = new Command("Nano", Command.SCREEN, 2), CLEAR = new Command("Clear", Command.SCREEN, 3), HISTORY = new Command("History", Command.SCREEN, 4),
+    private Command EXECUTE = new Command("Send", Command.OK, 0), 
+                    HELP = new Command("Help", Command.SCREEN, 1), 
+                    NANO = new Command("Nano", Command.SCREEN, 2), 
+                    CLEAR = new Command("Clear", Command.SCREEN, 3), 
+                    HISTORY = new Command("History", Command.SCREEN, 4),
                     BACK = new Command("Back", Command.BACK, 1), RUNS = new Command("Run Script", Command.OK, 1), IMPORT = new Command("Import File", Command.OK, 1), VIEW = new Command("View as HTML", Command.OK, 1),
                     RUN = new Command("Run", Command.OK, 1), EDIT = new Command("Edit", Command.OK, 1), OPEN = new Command("Open", Command.OK, 1), DELETE = new Command("Delete", Command.OK, 2);
     // |
