626f5fb487d65b2af492f39c94fd9d17a8c5b665
Lima <felipebr4095@gmail.com>
2025-08-15 06:41:09 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -367,7 +367,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
                 echoCommand(args[i] + " " + i);
                 if (args[i].equals("-a")) { all = true; } 
                 else if (args[i].equals("-v")) { verbose = true; } 
-                else { argument = join(args, " ", i); break; }
+                else { argument = join(args, " ", i++); break; }
             }
 
             String PWD = argument.equals("") ? path : argument; 
