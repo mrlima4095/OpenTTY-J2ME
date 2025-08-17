a8dcdff2005908a284461e34bbaa05e0b3af3c90
Lima <felipebr4095@gmail.com>
2025-08-17 11:57:48 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -1642,11 +1642,10 @@ public class OpenTTY extends MIDlet implements CommandListener {
                 else { return ret; }
             }
             else if (type.equals("while")) {
-                String expr = subst(PID, (String) cmd.get("expr"), vars, program, root);
-                while (eval(PID, expr, vars, program, root)) {
+                
+                while (eval(PID, subst(PID, (String) cmd.get("expr"), vars, program, root), vars, program, root)) {
                     String ret = C2ME(PID, (Vector) cmd.get("source"), context, program, root, 1);
-                    expr = subst(PID, (String) cmd.get("expr"), vars, program, root);
-
+                    
                     if (ret == null) { break; }
                     else if (ret.equals("+[continue]")) { continue; }
                     else { return ret; }
