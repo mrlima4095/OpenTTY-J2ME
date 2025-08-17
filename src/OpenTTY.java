3a163352d7fd22c0cc4cc6b5abe101912fae5e34
Mr Lima <felipebr4095@gmail.com>
2025-08-17 15:24:39 -0300

    -- PKI

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -361,62 +361,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("report")) { return processCommand("open mailto:felipebr4095@gmail.com"); }
         else if (mainCommand.equals("mail")) { echoCommand(request(getAppProperty("MIDlet-Proxy") + "raw.githubusercontent.com/mrlima4095/OpenTTY-J2ME/main/assets/root/mail.txt")); } 
         else if (mainCommand.equals("netstat")) { if (argument.equals("")) { argument = "http://ipinfo.io"; } int STATUS = 0; try { HttpConnection CONN = (HttpConnection) Connector.open(!argument.startsWith("http://") && !argument.startsWith("https://") ? "http://" + argument : argument); CONN.setRequestMethod(HttpConnection.GET); if (CONN.getResponseCode() == HttpConnection.HTTP_OK) { } else { STATUS = 101; } CONN.close(); } catch (SecurityException e) { STATUS = 13; } catch (Exception e) { STATUS = 101; } echoCommand(STATUS == 0 ? "true" : "false"); return STATUS; }
-        // |
-        // Certificates
-        else if (mainCommand.equals("pki")) {
-            if (argument.equals("")) { }
-            else {
-                String content = getcontent(argument);
-                if (content.equals("")) { }
-                else {
-                    int count = 0, idx = 0;
-                    while (true) {
-                        int beg = content.indexOf("-----BEGIN CERTIFICATE-----", idx);
-                        if (beg < 0) { break; }
-                        int end = content.indexOf("-----END CERTIFICATE-----", beg);
-                        if (end < 0) { break; }
-
-                        String base64 = content.substring(beg + "-----BEGIN CERTIFICATE-----".length(), end);
-                        StringBuffer sbNoNl = new StringBuffer();
-                        for (int i = 0; i < base64.length(); i++) {
-                            char c = base64.charAt(i);
-                            if (c!='\r' && c!='\n') sbNoNl.append(c);
-                        }
-                        String b64 = sbNoNl.toString().trim();
-
-                        final String B64CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-                        int[] T = new int[256]; for (int i=0;i<256;i++) T[i] = -1;
-                        for (int i=0;i<B64CHARS.length();i++) T[B64CHARS.charAt(i)] = i;
-                        T['='] = -2;
-                        java.io.ByteArrayOutputStream baos = new java.io.ByteArrayOutputStream();
-                        int val = 0, valb = -8;
-                        for (int i=0;i<b64.length();i++) {
-                            int ch = b64.charAt(i);
-                            if (ch==' '||ch=='\t') continue;
-                            if (ch=='\r'||ch=='\n') continue;
-                            int d = (ch<256)?T[ch]:-1;
-                            if (d == -1) continue;
-                            if (d == -2) break;
-                            val = (val << 6) + d;
-                            valb += 6;
-                            if (valb >= 0) {
-                                baos.write((val >> valb) & 0xFF);
-                                valb -= 8;
-                            }
-                        }
-                        byte[] der = baos.toByteArray();
-
-                        count++;
-                        echoCommand("cert#" + count + ": DER " + der.length + " bytes");
-                        idx = end + "-----END CERTIFICATE-----".length();
-                    }
-
-                    if (count == 0) { echoCommand("pki: " + argument + ": don't have PEM certs"); } 
-                    else { echoCommand("pki: found " + count + " PEM certs"); }
-                }
-            }       
-        }
-
+        
         // API 012 - (File)
         // |
         // Directories Manager
