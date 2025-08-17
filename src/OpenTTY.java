3e62589910e44c8ffd9e973689b52a9fe62be727
Lima <felipebr4095@gmail.com>
2025-08-16 22:55:51 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -84,7 +84,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
             } else {
                 preview.addCommand(BACK); 
                 
-                preview.addCommand(MOD == EXPLORER ? OPEN : MOD == PROCESS ? KILL : MOD == HISTORY ? RUN : LOAD);
+                preview.addCommand(MOD == EXPLORER ? OPEN : MOD == PROCESS ? KILL : MOD == HISTORY ? RUN : LOADS);
                 if (MOD == HISTORY) { preview.addCommand(EDIT); } 
     
                 preview.setCommandListener(this); 
