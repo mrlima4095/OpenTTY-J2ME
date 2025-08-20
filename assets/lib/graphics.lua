819543960e3f8828ccf20ec57b85766f363a50cd
Mr Lima <felipebr4095@gmail.com>
2025-08-19 21:38:40 -0300

    -- Screen Saving (class)

--- assets/lib/graphics.lua
+++ assets/lib/graphics.lua
@@ -1,5 +1,7 @@
 local graphics = {}
 
+
+
 function graphics.Alert(title, content)
 	return exec("warn " + title + " " + content)
 end
@@ -8,16 +10,16 @@ function graphics.SetTitle(title)
 	return exec("x11 title " + title)
 end
 
-function graphics.LoadScreen(name)
-	return exec("x11 load " + name);
+-- Screen Saving 
+function graphics.SaveWindow(name)
+	return exec("x11 set " + name)
+end
+function graphics.DropWindow(name)
+	return exec("x11 unset " + name)
+end
+function graphics.LoadWindow(name)
+	return exec("x11 load " + name)
 end
 
-function graphics.SaveScreen(name)
-	return exec("x11 set " + name);
-end
-
-function graphics.DropScreen(name)
-	return exec("x11 unset " + name);
-end
 
 return graphics
