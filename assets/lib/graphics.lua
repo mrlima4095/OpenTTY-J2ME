700018dedbd5733848ecacc05298bdb0529c11e1
Lima <felipebr4095@gmail.com>
2025-08-21 13:49:26 -0300

    update graphics.lua

--- assets/lib/graphics.lua
+++ assets/lib/graphics.lua
@@ -2,26 +2,26 @@ local graphics = {}
 
 
 function graphics.Alert(message)
-	return exec("warn " + title + " " + content)
+	return exec("warn " .. message)
 end
 
 -- Screen MODs
 function graphics.SetTitle(title)
-	return exec("x11 title " + title)
+	return exec("x11 title " .. message)
 end
 function graphics.SetTicker(text)
-	return exec("x11 tick " + text)
+	return exec("x11 tick " .. text)
 end
 
 -- Screen Saving 
 function graphics.SaveWindow(name)
-	return exec("x11 set " + name)
+	return exec("x11 set " .. name)
 end
 function graphics.DropWindow(name)
-	return exec("x11 unset " + name)
+	return exec("x11 unset " .. name)
 end
 function graphics.LoadWindow(name)
-	return exec("x11 load " + name)
+	return exec("x11 load " .. name)
 end
 
 
