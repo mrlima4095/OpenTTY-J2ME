17213227c66e1d9e31f4c76b509ce98d03456970
Mr Lima <felipebr4095@gmail.com>
2025-08-22 08:03:56 -0300

    Lua [ "os.pid" ]

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -1735,7 +1735,7 @@ class Lua {
     private Object unwrap(Object v) { return v == LUA_NIL ? null : v; }
 
     // Token types
-    public static final int PRINT = 0, EXEC = 1, ERROR = 2, PCALL = 3, GETENV = 4, REQUIRE = 5;
+    public static final int PRINT = 0, EXEC = 1, ERROR = 2, PCALL = 3, GETENV = 4, REQUIRE = 5, CLOCK = 6;
     private static final int EOF = 0, NUMBER = 1, STRING = 2, BOOLEAN = 3, NIL = 4, IDENTIFIER = 5, PLUS = 6, MINUS = 7, MULTIPLY = 8, DIVIDE = 9, MODULO = 10, EQ = 11, NE = 12, LT = 13, GT = 14, LE = 15,  GE = 16, AND = 17, 
                          OR = 18, NOT = 19, ASSIGN = 20, IF = 21, THEN = 22, ELSE = 23, END = 24, WHILE = 25, DO = 26, RETURN = 27, FUNCTION = 28, LPAREN = 29, RPAREN = 30, COMMA = 31, LOCAL = 32, LBRACE = 33, RBRACE = 34, 
                          LBRACKET = 35, RBRACKET = 36, CONCAT = 37, DOT = 38, ELSEIF = 39;
@@ -1750,6 +1750,7 @@ class Lua {
         Hashtable os = new Hashtable();
         os.put("execute", new MIDletLuaFunction(EXEC));
         os.put("getenv", new MIDletLuaFunction(GETENV));
+        os.put("pid", PID);
 
         globals.put("os", os);
         globals.put("print", new MIDletLuaFunction(PRINT));
