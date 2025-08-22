74040ec66b7b27340ae721976dd1764ffedf35f9
Mr Lima <felipebr4095@gmail.com>
2025-08-21 22:06:38 -0300

    os.execute

--- assets/lib/graphics.lua
+++ assets/lib/graphics.lua
@@ -2,14 +2,14 @@ local graphics = {}
 
 graphics["version"] = "1.0"
 
-function graphics.Alert(message) return exec("warn " .. message) end
-function graphics.Gauge(message) return exec("x11 gauge" .. message) end
+function graphics.Alert(message) return os.execute("warn " .. message) end
+function graphics.Gauge(message) return os.execute("x11 gauge" .. message) end
 
 -- Screen MODs
-function graphics.SetTitle(text) return exec("title " .. text) end
-function graphics.WindowTitle(text) return exec("x11 title" .. text) end
+function graphics.SetTitle(text) return os.execute("title " .. text) end
+function graphics.WindowTitle(text) return os.execute("x11 title" .. text) end
 
-function graphics.SetTicker(message) return exec("x11 tick " .. text) end
+function graphics.SetTicker(message) return os.execute("x11 tick " .. text) end
 
 
 
