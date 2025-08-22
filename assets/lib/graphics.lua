822a0755b010b1648d32655806eb081a648bb947
Lima <felipebr4095@gmail.com>
2025-08-22 12:36:11 -0300

    update graphics.lua

--- assets/lib/graphics.lua
+++ assets/lib/graphics.lua
@@ -3,11 +3,11 @@ local graphics = {}
 graphics["version"] = "1.0"
 
 function graphics.Alert(message) return os.execute("warn " .. message) end
-function graphics.Gauge(message) return os.execute("x11 gauge" .. message) end
+function graphics.Gauge(message) return os.execute("x11 gauge " .. message) end
 
 -- Screen MODs
 function graphics.SetTitle(text) return os.execute("title " .. text) end
-function graphics.WindowTitle(text) return os.execute("x11 title" .. text) end
+function graphics.WindowTitle(text) return os.execute("x11 title " .. text) end
 
 function graphics.SetTicker(message) return os.execute("x11 tick " .. message) end
 
