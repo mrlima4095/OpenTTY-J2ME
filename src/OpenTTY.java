5e44a340618ca2315b4ce35872a627d154045903
Mr Lima <felipebr4095@gmail.com>
2025-08-23 12:32:15 -0300

    +-- return lua;

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -564,7 +564,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         
         else if (mainCommand.equals("lua")) { 
             Lua lua = new Lua(this, root);
-            lua.run(argument, argument.equals("") ? nanoContent : getcontent(argument)); 
+            return lua.run(argument, argument.equals("") ? nanoContent : getcontent(argument)); 
         }
         else if (mainCommand.equals("5k")) { echoCommand("1.16 Special - 5k commits at OpenTTY GitHub repository"); }
 
