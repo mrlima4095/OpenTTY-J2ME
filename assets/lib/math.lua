49a2a3666e0a26c8fdb253492a6467685f39c034
Mr Lima <felipebr4095@gmail.com>
2025-08-23 15:23:24 -0300

    Math API - Using Runtime [ pow ... ]

--- assets/lib/math.lua
+++ assets/lib/math.lua
@@ -1,19 +1,9 @@
 local math = {}
 
-function math.abs(x) return x < 0 and 0-x or x end
-function math.sqrt(x) if x < 0 then error("math.sqrt expects a non-negative number") end return math.pow(x, 0.5) end
+function math.abs(x) return x < 0 and -x or x end
+function math.sqrt(x) if x < 0 then error("math.sqrt expects a non-negative number") end return x ^ 0.5 end
 
-function math.pow(base, exponent) 
-    if exponent == 0 then
-        return 1
-    else 
-        for i = 1, exponent do
-            base = base * base
-        end
-        
-        return base
-    end
-end
+function math.pow(base, exponent) return base ^ exponent end
 function math.factorial(n) if n == 0 then return 1 else return n * math.factorial(n - 1) end end
 
 return math
