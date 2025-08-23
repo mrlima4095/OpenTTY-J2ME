4c44c9f1d3f70100b0fd7c04c70d65b67493e8a6
Mr Lima <felipebr4095@gmail.com>
2025-08-23 16:28:51 -0300

    Math - ABS

--- assets/lib/math.lua
+++ assets/lib/math.lua
@@ -1,6 +1,12 @@
 local math = {}
 
-function math.abs(x) return x < 0 and -x or x end
+function math.abs(x) 
+	if x < 0 then
+		return x * 2
+	else 
+		return x
+	end
+end
 function math.sqrt(x) if x < 0 then error("math.sqrt expects a non-negative number") end return x ^ 0.5 end
 
 function math.pow(base, exponent) return base ^ exponent end
