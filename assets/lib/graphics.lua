273e9f75ecd42d08e728b36b26b59dbe19e469a9
Mr Lima <felipebr4095@gmail.com>
2025-08-23 10:27:35 -0300

    Graphics - Window Managing

--- assets/lib/graphics.lua
+++ assets/lib/graphics.lua
@@ -11,5 +11,11 @@ function graphics.WindowTitle(text) return os.execute("x11 title " .. text) end
 
 function graphics.SetTicker(message) return os.execute("x11 tick " .. message) end
 
+-- Screen Stacking
+function graphics.SaveWindow(name) return os.execute("x11 set " + name) end
+function graphics.DropWindow(name) return os.execute("x11 unset " + name) end
+function graphics.LoadWindow(name) return os.execute("x11 load " + name) end
+function graphics.LoadProcess(name) return os.execute("x11 import " + name) end
+
 
 return graphics
