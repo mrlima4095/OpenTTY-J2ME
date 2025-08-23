59ace03c263f2dd42559b4a981b9a9fc6d102527
Mr Lima <felipebr4095@gmail.com>
2025-08-22 22:02:18 -0300

    Math - added math.factorial

--- assets/lib/math.lua
+++ assets/lib/math.lua
@@ -4,5 +4,6 @@ function math.abs(x) return x < 0 and -x or x end
 function math.sqrt(x) if x < 0 then error("math.sqrt expects a non-negative number") end return x ^ 0.5 end
 
 function math.pow(base, exponent) return base ^ exponent end
+function math.factorial(n) if n == 0 then return 1 else return n * factorial(n - 1) end end
 
 return math
