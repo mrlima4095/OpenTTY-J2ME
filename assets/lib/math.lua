32e63842794904e51d58c3f449bbce99d20e580b
Lima <felipebr4095@gmail.com>
2025-08-25 09:10:49 -0300

    update math.lua

--- assets/lib/math.lua
+++ assets/lib/math.lua
@@ -1,6 +1,6 @@
 local math = { pi = 3.14159, randomseed = os.clock() }
 
-function math.abs(x) return x < 0 and -x or x end
+function math.abs(x) if x < 0 then return 0-(x) end return x end
 function math.sqrt(x) if x < 0 then error("math.sqrt expects a non-negative number") end return x ^ 0.5 end
 
 function math.pow(base, exponent) return base ^ exponent end
