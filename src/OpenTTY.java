e39c2ead4aa2c7ed803cd3ad453442466c57b27d
Mr Lima <felipebr4095@gmail.com>
2025-08-24 22:29:41 -0300

    Lua - collectgarbage()

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -2799,7 +2799,7 @@ class Lua {
             return returnValue;
         }
         public Object internals(Vector args) throws Exception {
-            if (MOD == PRINT || MOD == EXEC) { if (args.isEmpty()) { } else { return midlet.processCommand((MOD == PRINT ? "echo " : "") + toLuaString(args.elementAt(0)), true, root); } }
+            if (MOD == PRINT || MOD == EXEC || MOD == GC) { if (args.isEmpty()) { } else { return midlet.processCommand(MOD == GC ? "gc" : (MOD == PRINT ? "echo " : "") + toLuaString(args.elementAt(0)), true, root); } }
             else if (MOD == ERROR) { String msg = toLuaString((args.size() > 0) ? args.elementAt(0) : null); throw new Exception(msg.equals("nil") ? "error" : msg); } 
             else if (MOD == PCALL) {
                 Vector result = new Vector();
@@ -2908,7 +2908,6 @@ class Lua {
                     else { midlet.writeRMS(out, mode ? midlet.getcontent(out) + content : content); }
                 }
             }
-            else if (MOD == GC) { System().gc(); }
 
             return null;
         }
