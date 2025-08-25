4bc0b885872604d91cb7fc3469285e7a1f558867
Mr Lima <felipebr4095@gmail.com>
2025-08-25 10:39:45 -0300

    +-- test.lua

---
+++ assets/lib/teste.lua
@@ -0,0 +1,36 @@
+local g = require("graphics.lua")
+
+function load() 
+    local state = os.getenv("ESTADO")
+
+    if state == nil then
+        main()
+    elseif state == "PERGUNTAR" then
+        quest()
+    else 
+        local text = os.getenv("TEXT_LUA")
+
+        if text == nil then
+            g.Alert("Você não digitou nada ainda!")
+        else
+            g.Alert(text)
+        end
+    end
+end
+
+function main()
+    local tela = {
+        title = "Lista em Lua",
+        itens = {
+            ["Ler algo"] = "execute set ESTADO = PERGUNTAR; lua teste.lua; true"
+            ["Ver o que foi digitado"] = "execute set ESTADO = MOSTRAR; lua teste.lua; true"
+        }
+    }
+
+    g.BuildList(tela)
+
+function quest()
+    os.execute("execute install nano; add quest.title=Perguntando; add quest.label=Digite algo; quest.key=TEXT_LUA; quest.cmd=exec unset ESTADO & lua teste.lua")
+end
+
+load()
