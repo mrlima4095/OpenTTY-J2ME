709a540667089d994973a323c884c15b87cf88e8
Lima <felipebr4095@gmail.com>
2025-08-26 11:07:53 -0300

    update edit.lua

--- assets/lib/edit.lua
+++ assets/lib/edit.lua
@@ -59,7 +59,14 @@ local function load()
     end
 end
 
-local function main() edit["content"] = os.getenv("LEDIT_TXT") if edit["content"] == nil edit["content"] = "" end  g.BuildEdit(edit) end
+local function main() 
+    edit["content"] = os.getenv("LEDIT_TXT") 
+    if edit["content"] == nil then 
+        edit["content"] = "" 
+    end
+    
+    g.BuildEdit(edit)
+end
 local function save(filename)
     if filename == nil then
         explore["label"] = "(Save) File name",
