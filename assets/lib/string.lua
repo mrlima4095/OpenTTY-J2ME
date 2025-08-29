e88d62064b96dac7755fd991bb766bf207f40960
Mr Lima <felipebr4095@gmail.com>
2025-08-29 07:57:31 -0300

    String - trimming v2

--- assets/lib/string.lua
+++ assets/lib/string.lua
@@ -1,12 +1,20 @@
 function string.trim(text)
-    local buffer = ""
+    local buffer, x, y = "", 1, 1
 
-    for i=1,string.len(text) do
-        local cur = string.sub(text, i, i)
+    for i = 1, string.len(text) do
+        local char = string.sub(text, i, i)
 
-        if cur ~= " " or cur ~= "	" then
-            buffer = buffer .. cur
-        end
+        x = i
+        if char ~= " " or char ~= "	" then break end
+    end
+
+    text = string.reverse(text)
+
+    for i = 1, string.len(text) do
+        local char = string.sub(text, i, i)
+
+        y = i
+        if char ~= " " or char ~= "	" then break end
     end
 
     return buffer
