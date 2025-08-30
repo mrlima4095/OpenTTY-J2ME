7e3b835b6c16236ff666b6d691423aff03305da3
Mr Lima <felipebr4095@gmail.com>
2025-08-30 20:19:22 -0300

    Lua - Application Template

---
+++ build.lua
@@ -0,0 +1,38 @@
+local app = {
+    name = "appname",
+    version = "1.0",
+    description = "",
+
+    api = {
+        version = "1.16",
+        error = function ()
+            os.execute("")
+        end,
+        match = "minimum"
+    },
+
+    process = {
+        name = "pname",
+        exit = "execute echo Lua app have been killed",
+        type = "deamon",
+        host = "10141",
+        db = "luapp"
+    },
+
+    include = {},
+
+    build = function () os.execute("") end,
+    commands = {
+        name = "cmd alias"
+    },
+
+    shell = {
+        name = "cmd",
+        args = { },
+        unknown = ""
+    },
+
+    file = { }
+}
+
+return app
