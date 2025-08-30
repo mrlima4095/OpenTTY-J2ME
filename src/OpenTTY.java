83fbf0137be3bceb7f6e1769cd691bf87211bb0a
Mr Lima <felipebr4095@gmail.com>
2025-08-30 08:36:34 -0300

    string.hash - NullPointerException

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -3087,7 +3087,7 @@ class Lua {
                 }
             }
             else if (MOD == RANDOM) { return new Double(midlet.random.nextInt(midlet.getNumber(args.isEmpty() ? "100" : toLuaString(args.elementAt(0)), 100, false))); }
-            else if (MOD == HASH) { return args.isEmpty() || args.elementAt(0) == null ? new Double(args.elementAt(0).hashCode()) : null; }
+            else if (MOD == HASH) { return args.isEmpty() || args.elementAt(0) == null ? null : new Double(args.elementAt(0).hashCode()); }
             else if (MOD == BYTE) {
                 if (args.isEmpty() || args.elementAt(0) == null) { }
                 else {
