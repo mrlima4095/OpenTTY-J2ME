1676dc8ef306a017ae4acc45b62e80873a3b7558
Mr Lima <felipebr4095@gmail.com>
2025-09-04 19:32:46 -0300

    instanceOf OutputStream

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -3070,7 +3070,7 @@ class Lua {
                     String content = toLuaString(args.elementAt(0)), out = args.size() == 1 ? "stdout" : toLuaString(args.elementAt(1));
                     boolean mode = args.size() > 2 && toLuaString(args.elementAt(2)).equals("a") ? true : false;
 
-                    if (args.size() > 1 && args.elementAt(1) instanceof java.io.OutputStream) {
+                    if (args.size() > 1 && args.elementAt(1) instanceof OutputStream) {
                         OutputStream out = (OutputStream) args.elementAt(1);
 
                         out.write(content.getBytes("UTF-8")); out.flush();
