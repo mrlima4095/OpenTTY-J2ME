a56067768a984bf696ff480fe0e39cb7125087e3
Mr Lima <felipebr4095@gmail.com>
2025-09-04 21:49:52 -0300

    Lua - Server

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -1863,7 +1863,7 @@ class Lua {
     private Vector tokens;
     private int tokenIndex, status = 0, loopDepth = 0;
     // |
-    public static final int PRINT = 0, EXEC = 1, ERROR = 2, PCALL = 3, GETENV = 4, REQUIRE = 5, CLOCK = 6, EXIT = 7, SETLOC = 8, PAIRS = 9, READ = 10, WRITE = 11, GC = 12, TOSTRING = 13, TONUMBER = 14, UPPER = 15, LOWER = 16, LEN = 17, MATCH = 18, REVERSE = 19, SUB = 20, RANDOM = 21, LOADS = 22, HASH = 23, BYTE = 24, SELECT = 25, TYPE = 26, CHAR = 27, TB_DECODE = 28, TB_PACK = 29, CONNECT = 30, CLOSE = 31, HTTP_GET = 32, HTTP_POST = 33;
+    public static final int PRINT = 0, EXEC = 1, ERROR = 2, PCALL = 3, GETENV = 4, REQUIRE = 5, CLOCK = 6, EXIT = 7, SETLOC = 8, PAIRS = 9, READ = 10, WRITE = 11, GC = 12, TOSTRING = 13, TONUMBER = 14, UPPER = 15, LOWER = 16, LEN = 17, MATCH = 18, REVERSE = 19, SUB = 20, RANDOM = 21, LOADS = 22, HASH = 23, BYTE = 24, SELECT = 25, TYPE = 26, CHAR = 27, TB_DECODE = 28, TB_PACK = 29, CONNECT = 30, SERVER = 31, CLOSE = 32, HTTP_GET = 33, HTTP_POST = 34;
     public static final int EOF = 0, NUMBER = 1, STRING = 2, BOOLEAN = 3, NIL = 4, IDENTIFIER = 5, PLUS = 6, MINUS = 7, MULTIPLY = 8, DIVIDE = 9, MODULO = 10, EQ = 11, NE = 12, LT = 13, GT = 14, LE = 15,  GE = 16, AND = 17, OR = 18, NOT = 19, ASSIGN = 20, IF = 21, THEN = 22, ELSE = 23, END = 24, WHILE = 25, DO = 26, RETURN = 27, FUNCTION = 28, LPAREN = 29, RPAREN = 30, COMMA = 31, LOCAL = 32, LBRACE = 33, RBRACE = 34, LBRACKET = 35, RBRACKET = 36, CONCAT = 37, DOT = 38, ELSEIF = 39, FOR = 40, IN = 41, POWER = 42, BREAK = 43, LENGTH = 44, VARARG = 45, REPEAT = 46, UNTIL = 47;
     public static final Object LUA_NIL = new Object();
     // |
@@ -1891,7 +1891,7 @@ class Lua {
         funcs = new String[] { "get", "post" }; loaders = new int[] { HTTP_GET, HTTP_POST };
         for (int i = 0; i < funcs.length; i++) { http.put(funcs[i], new LuaFunction(loaders[i])); } socket.put("http", http);
 
-        funcs = new String[] { "connect" }; loaders = new int[] { CONNECT };
+        funcs = new String[] { "connect", "server" }; loaders = new int[] { CONNECT, SERVER };
         for (int i = 0; i < funcs.length; i++) { socket.put(funcs[i], new LuaFunction(loaders[i])); } globals.put("socket", socket);
 
         funcs = new String[] { "upper", "lower", "len", "match", "reverse", "sub", "hash", "byte", "char" }; loaders = new int[] { UPPER, LOWER, LEN, MATCH, REVERSE, SUB, HASH, BYTE, CHAR };
@@ -3277,6 +3277,33 @@ class Lua {
                 result.addElement(conn.openOutputStream());
 
                 return result;
+            }
+            else if (MOD == SOCKET_SERVER) {
+                if (args.isEmpty() || args.elementAt(0) == null) { return gotbad(1, "server", "number expected, got no value"); }
+                int port;
+                
+                try {
+                    Object arg = args.elementAt(0);
+                    if (arg instanceof Double) { port = ((Double) arg).intValue(); } 
+                    else { port = Integer.parseInt(toLuaString(arg)); }
+                } 
+                catch (Exception e) { return gotbad(1, "server", "number expected, got " + type(args.elementAt(0))); }
+
+                try {
+                    ServerSocketConnection server = (ServerSocketConnection) Connector.open("socket://:" + port);
+                    SocketConnection conn = (SocketConnection) server.acceptAndOpen();
+                    InputStream in = conn.openInputStream();
+                    OutputStream out = conn.openOutputStream();
+                    
+                    Vector result = new Vector();
+                    
+                    result.addElement(server);
+                    result.addElement(in);
+                    result.addElement(out);
+                    
+                    return result;
+                } 
+                catch (Exception e) { throw e; }
             }
             else if (MOD == HTTP_GET) { return args.isEmpty() || args.elementAt(0) == null ? gotbad(1, "get", "string expected, got no value") : request("GET", toLuaString(args.elementAt(0)), null); }
             else if (MOD == HTTP_POST) { return args.isEmpty() || args.elementAt(0) == null ? gotbad(1, "post", "string expected, got no value") : request("POST", toLuaString(args.elementAt(0)), args.size() > 1 ? toLuaString(args.elementAt(1)) : ""); }
