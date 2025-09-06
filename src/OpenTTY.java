496d325adf0a6b2300094f39cada0892d1d3f7f7
Lima <felipebr4095@gmail.com>
2025-09-06 10:55:59 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -3302,9 +3302,7 @@ class Lua {
                 return result;
             }
             else if (MOD == HTTP_GET || MOD == HTTP_POST) { return (args.isEmpty() || args.elementAt(0) == null ? gotbad(1, MOD == HTTP_GET ? "get" : "post", "string expected, got no value") : (MOD == HTTP_GET ? http("GET", toLuaString(args.elementAt(0)), null, args.size() > 1 ? (Hashtable) args.elementAt(1) : null) : http("POST", toLuaString(args.elementAt(0)), args.size() > 1 ? toLuaString(args.elementAt(1)) : "", args.size() > 2 ? args.elementAt(2) : null))); }            
-            else if (MOD == TRIM) {
-                return args.isEmpty() ? null : toLuaString(args.elementAt(0)).trim();
-            }
+            else if (MOD == TRIM) { return args.isEmpty() ? null : toLuaString(args.elementAt(0)).trim(); }
 
             return null;
         }
