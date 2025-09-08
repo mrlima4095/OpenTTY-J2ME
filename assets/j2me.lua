cabad8c7ca5d3257d936f32383175165572f6a69
Mr Lima <felipebr4095@gmail.com>
2025-09-08 13:12:18 -0300

    Code wrap

--- assets/j2me.lua
+++ assets/j2me.lua
@@ -55,29 +55,3 @@ graphics = {
     BuildEdit = function (config) end
 }
 
-
-
-
--- CONECTAR COM ALGO
-
-local conn, i, o = socket.connect("socket://opentty.xyz:31522")
-io.write("get /etc/passwd")
-print(io.read(i, 1024))
-io.close()
-
--- SERVIDOR
-
-local server = socket.server(1024)
-
-server.POST(function (context)
-    context:GetCookies()
-    context.Params:Get("name") -- pega o valor  da url ?name
-
-    context.ResponseHeaders = {'Content-type: text/html', 'Set-Cookie: fodase=sdlaksfd'}
-
-    return "<h1></h1>"
-end)
-
-
-server:Run()
-
