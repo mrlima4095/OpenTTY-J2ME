b88235584004a8c8bf2d4ccf7a782df7bff840af
Mr Lima <felipebr4095@gmail.com>
2025-09-08 13:11:32 -0300

    J2ME Lua API

--- assets/j2me.lua
+++ assets/j2me.lua
@@ -49,8 +49,35 @@ string = {
 
 graphics = {
     display = function (screen) end,
-    screen = function (config) end,
-    list = function (config) end,
-    quest = function (config) end,
-    edit = function (config) end
+    BuildScreen = function (config) end,
+    BuildList = function (config) end,
+    BuildQuest = function (config) end,
+    BuildEdit = function (config) end
 }
+
+
+
+
+-- CONECTAR COM ALGO
+
+local conn, i, o = socket.connect("socket://opentty.xyz:31522")
+io.write("get /etc/passwd")
+print(io.read(i, 1024))
+io.close()
+
+-- SERVIDOR
+
+local server = socket.server(1024)
+
+server.POST(function (context)
+    context:GetCookies()
+    context.Params:Get("name") -- pega o valor  da url ?name
+
+    context.ResponseHeaders = {'Content-type: text/html', 'Set-Cookie: fodase=sdlaksfd'}
+
+    return "<h1></h1>"
+end)
+
+
+server:Run()
+
