18f0b84cb3648da35a01c574286f38073f73a666
Mr Lima <felipebr4095@gmail.com>
2025-09-09 13:48:11 -0300

    QuestAPI

--- assets/lib/app.lua
+++ assets/lib/app.lua
@@ -1,8 +1,20 @@
-graphics.display(graphics.BuildEdit({
-    title = "Paste Content",
-    back = { root = app.PasteType },
-    button = {
-        label = "Create",
-        root = app.paste
-    }
-}))
+function QuestAPI()
+    graphics.display(graphics.BuildEdit({
+        title = "Insert your PasteBin API Key",
+
+        back = {
+            label = "Back",
+            root = os.exit
+        },
+
+        button = {
+            label = "Confirm",
+            root = function (input)
+                os.execute("set PASTEBIN_API=" .. input)
+                PASTEBIN_API = input
+            end
+        }
+    }))
+end
+
+QuestAPI()
