c82d2991562e317e8082460b0c522e13968370bc
Mr Lima <felipebr4095@gmail.com>
2025-09-11 21:46:11 -0300

    -- midlet.getenv [ local ]

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -1812,9 +1812,9 @@ class ItemLoader implements ItemCommandListener {
 
         if (!PKG.containsKey(node + ".label") || !PKG.containsKey(node + ".cmd")) { midlet.MIDletLogs("add error Malformed ITEM, missing params"); return; } 
 
-        RUN = new Command(midlet.getenv(node + ".label"), Command.ITEM, 1); 
-        s = new StringItem(null, midlet.getenv(node + ".label"), StringItem.BUTTON); 
-        s.setFont(midlet.newFont(midlet.getenv(node + ".style", "default"))); 
+        RUN = new Command(getenv(node + ".label"), Command.ITEM, 1); 
+        s = new StringItem(null, getenv(node + ".label"), StringItem.BUTTON); 
+        s.setFont(midlet.newFont(getenv(node + ".style", "default"))); 
         s.setLayout(Item.LAYOUT_EXPAND | Item.LAYOUT_NEWLINE_AFTER | Item.LAYOUT_NEWLINE_BEFORE); 
         s.addCommand(RUN); 
         s.setDefaultCommand(RUN); 
