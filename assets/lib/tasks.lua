8e495d3583bb91d551e603e82ee24b0b1bfb55b1
Mr Lima <felipebr4095@gmail.com>
2025-09-13 13:53:51 -0300

    Code wrap

--- assets/lib/tasks.lua
+++ assets/lib/tasks.lua
@@ -10,19 +10,19 @@ Export and Import tasks
 
 local function split(text, sep)
     local result, buffer, cur = {}, "", 1
-    
+
     for i = 1, string.len(text) do
         local char = string.sub(text, i, i)
-        
+
         if char == sep then
             result[cur] = buffer
             cur = cur + 1
             buffer = ""
         else buffer = buffer .. char end
     end
-    
+
     result[cur] = buffer
-    
+
     return result
 end
 
@@ -31,6 +31,6 @@ app.db = "/home/.tasks"
 
 function app.load()
     local content = io.read(app.db)
-    
-    local app.tasks = split(content, "\n")
+
+    app.tasks = split(content, "\n")
 end
