b0938be0360f3be4a70911c293ddce2879182c4d
Mr Lima <felipebr4095@gmail.com>
2025-09-12 21:08:59 -0300

    Code wrap

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -3279,9 +3279,7 @@ class Lua {
                     String process = toLuaString(args.elementAt(0)).trim();
 
                     if (midlet.trace.containsKey(process)) {
-                        if (((String) midlet.getobject(process, "owner")).equals("root") && !root) {
-                            return gotbad(1, "getproc", "permissiond denied");
-                        }
+                        if (((String) midlet.getobject(process, "owner")).equals("root") && !root) { return gotbad(1, "getproc", "permissiond denied"); }
 
                         if (args.size() > 1) { return (midlet.getprocess(process)).get(toLuaString(args.elementAt(1)).trim()); } 
                         else {
