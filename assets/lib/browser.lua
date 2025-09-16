6e93a62069b657bc0d8b95980615122853123ab3
Mr Lima <felipebr4095@gmail.com>
2025-09-16 11:57:56 -0300

    Browser

---
+++ assets/lib/browser.lua
@@ -0,0 +1,44 @@
+local browser = {
+    version = "1.0",
+
+    xback = os.exit,
+    xmenucfg = {
+        title = "Browser",
+        back = {  }
+    },
+
+    headers = {}
+}
+
+function browser.render(raw)
+    
+    return raw
+end
+function browser.load(url)
+    local raw, status = socket.http.get(url, browser.headers)
+
+    if status ~= 200 then raw = "<title>404 - Not found</title>\nPage not found" end
+
+    
+end
+
+function browser.quest()
+    graphics.display(graphics.BuildQuest({
+        title = "Browser",
+        label = "WebSite URL",
+        content = "http://",
+        back = { root = browser.main },
+        button = { label = "Go", root = browser.load }
+    }))
+end
+
+function browser.main()
+    if browser.menu == nil then browser.menu = graphics.BuildList(browser.menu) end
+
+    graphics.display(browser.menu)
+end
+
+
+
+os.setproc("name", "browser")
+browser.quest()
