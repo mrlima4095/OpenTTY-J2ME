734c841ccb9f29c595edb31ade5d4049ec043850
Mr Lima <felipebr4095@gmail.com>
2025-09-20 14:14:07 -0300

    Build "2025-1.16.1-02x77" - Release 1.16.1

--- assets/scripts/latest.sh
+++ assets/scripts/latest.sh
@@ -1,9 +1,9 @@
 #!/java/bin/sh
 # -*- coding: utf-8 -*-
 
-set ONLINE_VERSION=1.16
+set ONLINE_VERSION=1.16.1
 
 
 if ($VERSION == $ONLINE_VERSION) echo already up to date.
-if ($VERSION != $ONLINE_VERSION) execute echo A new version was released!; echo; echo Local - OpenTTY $VERSION; echo Server - OpenTTY $ONLINE_VERSION; open http://opentty.xyz/repo/versions/OpenTTY-$ONLINE_VERSION.jar;
+if ($VERSION != $ONLINE_VERSION) execute echo A new version was released!; echo; echo Local - OpenTTY $VERSION; echo Server - OpenTTY $ONLINE_VERSION; open http://opentty.xyz/versions/OpenTTY-$ONLINE_VERSION.jar;
 
