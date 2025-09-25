0849226705d4581e0db14d6329a1ab8cbbf7fec9
Mr Lima <felipebr4095@gmail.com>
2025-09-24 21:27:04 -0300

    httpd.lua - Code unwrap

--- assets/lib/httpd.lua
+++ assets/lib/httpd.lua
@@ -4,7 +4,9 @@ httpd._routes = {}
 httpd._row = 1
 
 local function trim(s)
-    if not s or #s == 0 then return "" end
+    if not s or #s == 0 then
+        return ""
+    end
     local start = 1
     while start <= #s and string.sub(s, start, start) == " " do
         start = start + 1
@@ -31,7 +33,9 @@ local function getMethod(raw)
     local buffer = ""
     for i = 1, #raw do
         local cur = string.sub(raw, i, i)
-        if cur == " " then break end
+        if cur == " " then
+            break
+        end
         buffer = buffer .. cur
     end
     return buffer
@@ -40,7 +44,9 @@ end
 local function getHeaders(raw)
     local headers = {}
     local pos = indexOf(raw, "\r\n", 1)
-    if not pos then return headers end
+    if not pos then
+        return headers
+    end
     pos = pos + 2
 
     local header_end = indexOf(raw, "\r\n\r\n", pos)
@@ -71,54 +77,70 @@ end
 
 local function getRoute(raw)
     local space1 = indexOf(raw, " ", 1)
-    if not space1 then return "" end
+    if not space1 then
+        return ""
+    end
     local start = space1 + 1
 
     local space2 = indexOf(raw, " ", start)
-    if not space2 then space2 = #raw + 1 end
+    if not space2 then
+        space2 = #raw + 1
+    end
 
     return string.sub(raw, start, space2 - 1)
 end
 
 local function getBody(raw)
     local end_req = indexOf(raw, "\r\n", 1)
-    if not end_req then return "" end
+    if not end_req then
+        return ""
+    end
     local pos = end_req + 2
 
     local header_end = indexOf(raw, "\r\n\r\n", pos)
-    if not header_end then return "" end
+    if not header_end then
+        return ""
+    end
 
     return string.sub(raw, header_end + 4)
 end
 
-function httpd.route(path, method, handler)
-    if not path then error("bad argument #1 for 'route'") end
+httpd.route = function(path, method, handler)
+    if not path then
+        error("bad argument #1 for 'route' (string expected, got no value)")
+    end
     if not handler then
         handler = method
         method = "GET"
     end
     httpd._routes[path] = { method = method, handler = handler }
 end
-
-function httpd.run(port, debug, buffer, mime)
+httpd.run = function (port, debug, buffer, mime)
     while true do
         local ok, server = pcall(socket.server, port)
         if not ok then
             pcall(io.close, server)
             print(server)
 
-            if httpd._row == 1 then error("httpd - server binding error")
-            else error("httpd - " .. server) end
+            if httpd._row == 1 then
+                error("httpd - server binding error")
+            else
+                error("httpd - " .. server)
+            end
         end
 
-        if httpd._row == 1 then print("httpd - listening at port " .. port) end
+        if httpd._row == 1 then
+            print("httpd - listening at port " .. port)
+        end
 
         local client, i, o = socket.accept(server)
         if client then
             local raw = io.read(i, buffer or 4096)
 
             if raw then
-                if debug then print(string.trim(raw)) end
+                if debug then
+                    print(trim(raw))
+                    end
 
                 local method, route, headers, body = getMethod(raw), getRoute(raw), getHeaders(raw), getBody(raw)
                 local handler = httpd._routes[route]
