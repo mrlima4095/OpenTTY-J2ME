c26d038631f584f611f1cf414531388f8d4f0da9
Mr Lima <felipebr4095@gmail.com>
2025-09-26 21:53:45 -0300

    Lua - Graphics Form API (testing script)

---
+++ assets/lib/form2.lua
@@ -0,0 +1,15 @@
+graphics.BuildScreen{
+    title = "Tela de Teste",
+    back = { label = "Voltar" },
+    button = { label = "OK" },
+    fields = {
+        [1] = { type = "text", label = "Mensagem:", value = "Olá mundo no J2ME!" },
+        [2] = { type = "textfield", label = "Digite algo", value = "" },
+        [3] = { type = "spacer", width = 1, heigth = 20 },
+        [4] = { type = "gauge", label = "Progresso", interactive = false, max = 100, value = 40 },
+        [5] = { type = "choice", label = "Opções", mode = "exclusive",
+                options = { [1] = "A", [2] = "B", [3] = "C" } },
+        [6] = { type = "item", label = "Executar", root = "echo Você clicou em Executar" }
+    }
+}
+graphics.display()
