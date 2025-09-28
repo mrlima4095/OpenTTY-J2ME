131624f761e8fa40b9e2ecc85d69e8300c22d08e
Lima <felipebr4095@gmail.com>
2025-09-28 17:54:56 -0300

    update browser.lua

--- assets/lib/browser.lua
+++ assets/lib/browser.lua
@@ -17,7 +17,7 @@ local function parse_html(html)
 
     while i <= #html do
         local start_tag = string.match(html, "<", i)
-        if not start_tag then
+        if start_tag == nil then
             if not in_head then
                 local text = string.trim(string.sub(html, i))
                 if text ~= "" then
