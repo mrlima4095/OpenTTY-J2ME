2cc8838f596f4457130ce0122b596314295b0e3b
Mr Lima <felipebr4095@gmail.com>
2025-09-28 14:05:58 -0300

    -- test case

--- assets/lib/browser.lua
+++ assets/lib/browser.lua
@@ -185,8 +185,5 @@ function browser.main()
     }))
 end
 
-print(extract_title("<title>teste</title>", "nao acho"))
-print(extract_title("<x>teste</x>", "nao acho"))
-
 if #arg == 1 then browser.main()
 else browser.load(arg[1]) end
