77faa91f312163ea86c7f9fb59d47238cbec8eda
Mr Lima <felipebr4095@gmail.com>
2025-09-29 08:31:16 -0300

    return expression(scope);

--- src/Lua.java
+++ src/Lua.java
@@ -763,7 +763,7 @@ class Lua {
         }
         else if (current.type == BREAK) { if (loopDepth == 0) { throw new RuntimeException("break outside loop"); } consume(BREAK); breakLoop = true; return null; }
         else if (current.type == END) { consume(END); return null; }
-        else if (current.type == LPAREN || current.type == NUMBER || current.type == STRING || current.type == BOOLEAN || current.type == NIL || current.type == NOT) { expression(scope); return null; }
+        else if (current.type == LPAREN || current.type == NUMBER || current.type == STRING || current.type == BOOLEAN || current.type == NIL || current.type == NOT) { return expression(scope); }
 
         throw new RuntimeException("Unexpected token at statement: " + current.toString() + " - " + peekNext().toString());
     }
