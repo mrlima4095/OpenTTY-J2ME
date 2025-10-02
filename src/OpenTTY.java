429224ddfee6638a7d4e7e4e5aa0e80cf64da066
Mr Lima <felipebr4095@gmail.com>
2025-10-02 18:46:39 -0300

    id

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -1308,7 +1308,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         // |
         // X11 Loader
         else if (mainCommand.equals("term")) { display.setCurrent(form); }
-        else if (mainCommand.equals("init")) { start("x11-wm", null, null, true); } 
+        else if (mainCommand.equals("init")) { start("x11-wm", null, null, id); } 
         else if (mainCommand.equals("stop")) { form.setTitle(""); form.setTicker(null); form.deleteAll(); xserver("cmd hide", id); form.removeCommand(EXECUTE); trace.remove("2"); }
         else if (mainCommand.equals("xfinit")) { if (argument.equals("")) { return xserver("init", id); } if (argument.equals("stdin")) { form.append(stdin); } else if (argument.equals("stdout")) { form.append(stdout); } }
         else if (mainCommand.equals("cmd")) { Command[] CMDS = { HELP, NANO, CLEAR, HISTORY }; for (int i = 0; i < CMDS.length; i++) { if (argument.equals("hide")) { form.removeCommand(CMDS[i]); } else { form.addCommand(CMDS[i]); } } }
