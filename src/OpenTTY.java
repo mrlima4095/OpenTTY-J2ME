84dadbc1e80e3fd280260c5e4478bc52df43a115
Mr Lima <felipebr4095@gmail.com>
2025-10-01 21:27:54 -0300

    Build "2025-1.17-02x87" - Run '.initrc' considering shebang

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -20,7 +20,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
     public Hashtable attributes = new Hashtable(), paths = new Hashtable(), trace = new Hashtable(), filetypes = null,
                      aliases = new Hashtable(), shell = new Hashtable(), functions = new Hashtable(), tmp = new Hashtable();
     public String username = loadRMS("OpenRMS"), nanoContent = loadRMS("nano");
-    public String logs = "", path = "/home/", build = "2025-1.17-02x86";
+    public String logs = "", path = "/home/", build = "2025-1.17-02x87";
     public Display display = Display.getDisplay(this);
     public TextBox nano = new TextBox("Nano", "", 31522, TextField.ANY);
     public Form form = new Form("OpenTTY " + getAppProperty("MIDlet-Version"));
@@ -448,7 +448,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
                         if (asking_passwd) { writeRMS("OpenRMS", String.valueOf(password.hashCode()).getBytes(), 2); }
 
                         display.setCurrent(form);
-                        runScript(loadRMS(".initrc"));
+                        processCommand(". /home/.initrc");
                         setLabel();
                     }
                 } 
