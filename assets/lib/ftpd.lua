c09640c15aeaeac23bbd168c771bf603d62f7434
Mr Lima <felipebr4095@gmail.com>
2025-10-04 17:18:30 -0300

    FTPD - Package requirements

--- assets/lib/ftpd.lua
+++ assets/lib/ftpd.lua
@@ -2,6 +2,17 @@
 
 --[[
 
+[ Config ]
+
+name=FTPd
+version=1.0
+description=J2ME FTP Server
+
+api.version=1.17
+api.require=lua
+api.error=execute echo [ FTPd ] Required OpenTTY 1.17 or newer and Lua Runtime 
+api.match=minimum
+
 ]]
 
 local app = {}
