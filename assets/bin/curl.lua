7aa457bc61936287eb5b909044a6b5d3f7178632
Mr Lima <felipebr4095@gmail.com>
2025-10-09 21:36:41 -0300

    curl application

--- assets/bin/curl.lua
+++ assets/bin/curl.lua
@@ -1,16 +1,14 @@
 #!/bin/lua
 
 local function parse_url(url)
-    if string.sub(url, 0, 4) == "http:" or string.sub(url, 0, 6) == "https:" then
-        return url
-    end
+    if string.sub(url, 1, 4) == "http:" or string.sub(url, 0, 6) == "https:" then return url end
     return "http://" .. url
 end
 
 if arg[1] then
     local url = parse_url(arg[1])
 
-    local body, status = socket.http.get(url)
-    if status == 200 then print(body)
-    else print("curl: " .. url .. " (status: " .. status .. ")") end
+    local ok, body, status = pcall(socket.http.get, url)
+    if ok then print(body)
+    else print("curl: (6) Could not resolve host: " .. arg[1]) end
 else print("curl [url]") end
