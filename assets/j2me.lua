a69c6cafd8052f183a74e9701ff5600dec468e54
Mr Lima <felipebr4095@gmail.com>
2025-10-11 16:40:01 -0300

    J2ME Lua API

--- assets/j2me.lua
+++ assets/j2me.lua
@@ -1,26 +1,33 @@
 os = {
-    execute = function (command) end,
-    getenv = function (key) end,
+    execute = function (command) return 0 end,
+    getenv = function (key) return nil or key end,
     clock = function () end,
     setlocale = function (locale) end,
-    exit = function (status) end,
+    exit = function (status) os.exit(status, true) end,
     date = function () end,
     getpid = function (proc) end,
-    getproc = function (pid, field) end,
-    setproc = function (field, value) end
+    getproc = function (pid, field) return {} end,
+    setproc = function (field, value) end,
+    getcwd = function () return "/home/" end
 }
 
 package = { loadlib = function (libname, funcname) end, loaded = {} }
 
 io = {
-    read = function (text, file) end,
+    read = function (file, lenght) end,
     write = function (text, file, mode) end,
     close = function (...) end
 }
 
 table = {
-    pack = function (...) end,
-    decode = function (text) end
+    insert = function (table, field) end,
+    concat = function (table, char) end,
+    remove = function (table, field) end,
+    sort = function (table) end,
+    move = function (table, from, to) end,
+    pack = function () end,
+    unpack = function () end,
+    decode = function (text) return {} end
 }
 
 socket = {
@@ -32,21 +39,23 @@ socket = {
     device = function (conn) end,
 
     http = {
-        get = function (url, headers) end,
-        post = function (url, data, headers) end
+        get = function (url, headers) return "", 200 end,
+        post = function (url, data, headers) return "", 200 end
     }
 }
 
 string = {
-    upper = function (text) end,
-    lower = function (text) end,
-    len = function (text) end,
-    match = function (text, pattern) end,
-    reverse = function (text) end,
-    sub = function (text, x, y) end,
-    hash = function (text) end,
-    byte = function (text, x, y) end,
-    char = function (...) end
+    upper = function (s) end,
+    lower = function (s) end,
+    len = function (s) end,
+    find = function (s, pattern, init, plain) end,
+    match = function (s, pattern) end,
+    reverse = function (s) end,
+    sub = function (s, x, y) end,
+    hash = function (s) end,
+    byte = function (s, x, y) end,
+    char = function (...) end,
+    trim = function (s) return "" end
 }
 
 graphics = {
@@ -57,8 +66,17 @@ graphics = {
     BuildQuest = function (config) end,
     BuildEdit = function (config) end,
     SetTitle = function (title) end,
-    WindowTitle = function (title) end,
-    SetTicker = function (text) end
+    WindowTitle = function (title, screen) end,
+    SetTicker = function (text) end,
+    getCurrent = function () end,
+    render = function (img) end,
+    append = function (screen, field) end,
+    xterm = ""
+}
+
+java = {
+    class = function (name) return true end,
+    getName = function () return "JVM" end
 }
 
 getAppProperty = function (field) end
