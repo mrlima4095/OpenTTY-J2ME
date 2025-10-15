f13583e476eebc1b15faa0b5fff381c39b8a1eb4
Mr Lima <felipebr4095@gmail.com>
2025-10-15 09:48:46 -0300

    Improved command 'which' to recognize /bin/ applications

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -1136,7 +1136,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         else if (mainCommand.equals("true") || mainCommand.startsWith("#")) { }
         else if (mainCommand.equals("false")) { return 255; }
 
-        else if (mainCommand.equals("which")) { if (argument.equals("")) { } else { echoCommand(shell.containsKey(argument) ? "shell" : (aliases.containsKey(argument) ? "alias" : (functions.containsKey(argument) ? "function" : ""))); } }
+        else if (mainCommand.equals("which")) { if (argument.equals("")) { } else { echoCommand(shell.containsKey(argument) ? "shell" : (aliases.containsKey(argument) ? "alias" : (functions.containsKey(argument) ? "function" : file("/bin/" + mainCommand) ? "application" : ""))); } }
 
         // API 014 - (OpenTTY)
         // |
