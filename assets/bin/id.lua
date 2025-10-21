82691fdc6479822819ac69ce91ed02abc9019281
Mr Lima <felipebr4095@gmail.com>
2025-10-21 12:53:30 -0300

    Code wrap

--- assets/bin/id.lua
+++ assets/bin/id.lua
@@ -1,7 +1,4 @@
 local request = arg[1]
 local id = os.execute("case user (" .. request .. ") false")
 if id == 255 then return true
-else
-    print(arg[0] .. ": '" .. request .. "': no such user")
-    return false
-end
+else print(arg[0] .. ": '" .. request .. "': no such user") return false end
