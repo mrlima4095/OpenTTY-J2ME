a24f443808c2c68ad0232fce93a3e2e9b03559cf
Mr Lima <felipebr4095@gmail.com>
2025-10-22 20:00:34 -0300

    HTTPS Proxy

--- assets/proxy.php
+++ assets/proxy.php
@@ -1,14 +1,17 @@
 <?php
-$url = urldecode($_SERVER['QUERY_STRING'] ?? '');
+$url = $_SERVER['QUERY_STRING'] ?? '';
+
+if (strpos($url, 'url=') === 0) { $url = substr($url, 4); }
+
+$url = urldecode($url);
 
 if (empty($url)) {
     http_response_code(400);
+    echo "Error: No URL provided";
     exit;
 }
 
-if (!preg_match('#^https?://#i', $url)) {
-    $url = 'https://' . $url;
-}
+if (!preg_match('#^https?://#i', $url)) { $url = 'https://' . $url; }
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -16,9 +19,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
-if (isset($_SERVER['HTTP_USER_AGENT'])) {
-    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-}
+if (isset($_SERVER['HTTP_USER_AGENT'])) { curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); }
 
 $response = curl_exec($ch);
 $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
