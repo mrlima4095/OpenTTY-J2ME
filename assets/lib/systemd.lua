7c459f0946036ca97153ab99a74d7dfb6a1f2648
Lima <felipebr4095@gmail.com>
2025-10-23 22:31:21 -0300

    update systemd.lua

--- assets/lib/systemd.lua
+++ assets/lib/systemd.lua
@@ -1,9 +1,26 @@
 #!/bin/lua
 
+
+local function menu(opt) 
+    graphics.display(graphics.BuildList({
+        back = { root = function() main() end },
+        button = {
+            label = "Select",
+            root = function(sel)
+                if sel == "Add" then
+                    os.execute("warn Will open a Quest Window")
+                elseif sel == "Delete" then
+                    os.execute("warn Deleted " .. opt)
+                end
+            end
+        }
+    }))
+end 
 local screen = {
     title = "Services",
     back = { root = os.exit },
-    button = { label = "" }
+    button = { label = "Menu", root = menu },
+    fields = { }
 }
 local function handler()
     
@@ -11,6 +28,12 @@ end
 local function main()
     
 end
+
+if os.execute("case user (root) false") ~= 255 then
+    print("Permission denied!")
+    os.exit(13)
+end
+
 
 os.setproc("name", "systemd")
 main()
