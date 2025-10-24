2d2d9f984d32a895bf8e7d330c17177d7d60529b
Mr Lima <felipebr4095@gmail.com>
2025-10-23 21:58:01 -0300

    SystemD - Manage OpenTTY Services

---
+++ assets/lib/systemd.lua
@@ -0,0 +1,16 @@
+#!/bin/lua
+
+local screen = {
+    title = "Services",
+    back = { root = os.exit },
+    button = { label = "" }
+}
+local function handler()
+    
+end
+local function main()
+    
+end
+
+os.setproc("name", "systemd")
+main()
