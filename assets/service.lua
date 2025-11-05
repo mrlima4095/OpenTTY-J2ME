f36b61a93b6bf06b7a533a761ed242b65c0b03a4
Mr Lima <felipebr4095@gmail.com>
2025-11-05 15:13:17 -0300

    it's our

---
+++ assets/service.lua
@@ -0,0 +1,21 @@
+return function (payload, source, pid, id)
+    -- payload: requistion for the service (on Lua can be any value, from shell only strings)
+    -- source: [lua, mod, shell] where this request was called?
+    -- pid: process id of who make this request
+    -- id: user id of who make this request
+
+    print("---")
+    print("Debugger Service")
+    print("* Payload: " .. payload)
+    print("* Source: " .. source)
+    print("* Request from (PID): " .. pid)
+    print("* Who calls? (UID): " .. id)
+    print("---")
+
+    if payload == "ping" then
+        print("pong")
+    elseif payload == nil then
+        print("nothing to process")
+    end
+
+end
