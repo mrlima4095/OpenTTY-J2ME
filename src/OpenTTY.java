90fa344f1ae5c11558770a6311ab35abe525d727
Mr Lima <felipebr4095@gmail.com>
2025-11-21 12:52:52 -0300

    { ... }

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -1485,7 +1485,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
         } else if (content.startsWith("[ Config ]")) { return importScript(content, id, stdout, scope);
         } else { return runScript(content, id, pid, stdout, scope); }
     }
-    public Object goLua(String argument, String[] args, int id, String pid, String stdout, Hashtable scope)
+    public Object goLua(String argument, String[] args, int id, String pid, String stdout, Hashtable scope) { }
 }            
 // |
 // EOF
