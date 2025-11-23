76df51961c7570103077feed6986542aab466d52
Lima <felipebr4095@gmail.com>
2025-11-22 21:49:30 -0300

    update expr.lua

--- assets/lib/expr.lua
+++ assets/lib/expr.lua
@@ -2,8 +2,4 @@
 
 os.setproc("name", "expr")
 
-if arg[1] then
-    print(load("return " .. arg[1]))
-else
-    print("expr: missing operand")
-end
+if arg[1] then print(load("return " .. arg[1])) else print("expr: missing operand") end
