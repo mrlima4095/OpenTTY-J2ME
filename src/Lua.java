0e53468dee440837131a82b28bd721750f94e863
Mr Lima <felipebr4095@gmail.com>
2025-11-24 21:57:55 -0300

    StringItem

--- src/Lua.java
+++ src/Lua.java
@@ -1965,7 +1965,7 @@ public class Lua {
                         else if (type.equals("spacer")) { form.append(new Spacer(getFieldNumber(field, "width", 1), getFieldNumber(field, "height", 10))); }
                         else if (type.equals("gauge")) { form.append(new Gauge(getFieldValue(field, "label", ""), getFieldBoolean(field, "interactive", false), getFieldNumber(field, "maxValue", 100), getFieldNumber(field, "value", 0))); } 
                     } 
-                    else if (itemObj instanceof StringItem) { form.append((Item) itemObj); } 
+                    else if (itemObj instanceof StringItem) { form.append((StringItem) itemObj); } 
                     else { form.append(new StringItem("", toLuaString(itemObj))); }
                 } else if (target instanceof List) {
                     List list = (List) target;
