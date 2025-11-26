8fc2dbb1ff25278a128938f9e63d01103c9d30f9
Mr Lima <felipebr4095@gmail.com>
2025-11-25 21:15:55 -0300

    lua.toString();

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -44,8 +44,8 @@ public class OpenTTY extends MIDlet {
             Hashtable proc = new Hashtable(), args = new Hashtable(); args.put(new Double(0), "/bin/init"); globals.put("PWD", "/home/");
             proc.put("name", "init"); proc.put("owner", "root");
 
-            //Lua lua = new Lua(this, 0, "1", proc, stdout, globals); 
-            warn("hello", "working!");
+            Lua lua = new Lua(this, 0, "1", proc, stdout, globals); 
+            warn("hello", "working! " + lua.toString());
             /*sys.put("1", proc); lua.globals.put("arg", args);
             
             try { 
