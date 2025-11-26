d438d1b90112ac58d21d9cfe8851653e9653b483
Mr Lima <felipebr4095@gmail.com>
2025-11-26 11:36:53 -0300

    THREAD

--- src/Lua.java
+++ src/Lua.java
@@ -26,7 +26,7 @@ public class Lua {
     public static final int READ = 400, WRITE = 401, CLOSE = 402, OPEN = 403, POPEN = 404, DIRS = 405, SETOUT = 406, MOUNT = 407;
     public static final int HTTP_GET = 500, HTTP_POST = 501, CONNECT = 502, PEER = 503, DEVICE = 504, SERVER = 505, ACCEPT = 506;
     public static final int DISPLAY = 600, NEW = 601, RENDER = 602, APPEND = 603, ADDCMD = 604, HANDLER = 605, GETCURRENT = 606, TITLE = 607, TICKER = 608, VIBRATE = 609, LABEL = 610, SETTEXT = 611;
-    public static final int CLASS = 700, NAME = 701, DELETE = 702, UPTIME = 703;
+    public static final int CLASS = 700, NAME = 701, DELETE = 702, UPTIME = 703, THREAD = 704;
 
     public static final int EOF = 0, NUMBER = 1, STRING = 2, BOOLEAN = 3, NIL = 4, IDENTIFIER = 5, PLUS = 6, MINUS = 7, MULTIPLY = 8, DIVIDE = 9, MODULO = 10, EQ = 11, NE = 12, LT = 13, GT = 14, LE = 15,  GE = 16, AND = 17, OR = 18, NOT = 19, ASSIGN = 20, IF = 21, THEN = 22, ELSE = 23, END = 24, WHILE = 25, DO = 26, RETURN = 27, FUNCTION = 28, LPAREN = 29, RPAREN = 30, COMMA = 31, LOCAL = 32, LBRACE = 33, RBRACE = 34, LBRACKET = 35, RBRACKET = 36, CONCAT = 37, DOT = 38, ELSEIF = 39, FOR = 40, IN = 41, POWER = 42, BREAK = 43, LENGTH = 44, VARARG = 45, REPEAT = 46, UNTIL = 47, COLON = 48;
     public static final Object LUA_NIL = new Object();
@@ -1116,7 +1116,7 @@ public class Lua {
     private static boolean isLetterOrDigit(char c) { return isLetter(c) || isDigit(c); }
     // |
     // Lua Object
-    public class LuaFunction implements CommandListener, ItemCommandListener, ItemStateListener {
+    public class LuaFunction implements Runnable, CommandListener, ItemCommandListener, ItemStateListener {
         private Vector params, bodyTokens;
         private Hashtable closureScope, cmds = null; 
         private int MOD = -1;
@@ -1128,6 +1128,7 @@ public class Lua {
         LuaFunction(Vector params, Vector bodyTokens, Hashtable closureScope) { this.params = params; this.bodyTokens = bodyTokens; this.closureScope = closureScope; }
         LuaFunction(String handler, Object root) { this.handler = handler; this.root = root; }
         LuaFunction(Hashtable cmds) { this.cmds = cmds; }
+        LuaFunction(LuaFunction root) { this.root = root; }
         LuaFunction(int type) { this.MOD = type; }
         // | (Main)
         public Object call(Vector args) throws Exception {
