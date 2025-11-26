a8e0244b003d77b40d87af2f0a50324b084273b3
Mr Lima <felipebr4095@gmail.com>
2025-11-25 21:32:17 -0300

    Is Lua booting?

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -47,7 +47,7 @@ public class OpenTTY extends MIDlet {
 
                 Lua lua = new Lua(this, 0, "1", proc, stdout, globals); 
                 warn("hello", "working! but not booting");
-                sys.put("1", proc); lua.globals.put("arg", args);
+                //sys.put("1", proc); lua.globals.put("arg", args);
 
                 //lua.tokens = lua.tokenize(read("/bin/init")); 
                 
