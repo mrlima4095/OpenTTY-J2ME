2871bfb1a1c3ed5b5c3329f0331e36461a2efc6b
Mr Lima <felipebr4095@gmail.com>
2025-11-27 16:50:32 -0300

    ssh

--- src/Lua.java
+++ src/Lua.java
@@ -21,7 +21,7 @@ public class Lua {
     public static final int PRINT = 0, ERROR = 1, PCALL = 2, REQUIRE = 3, LOADS = 4, PAIRS = 5, GC = 6, TOSTRING = 7, TONUMBER = 8, SELECT = 9, TYPE = 10, GETPROPERTY = 11, SETMETATABLE = 12, GETMETATABLE = 13, IPAIRS = 14, RANDOM = 15;
     public static final int UPPER = 100, LOWER = 101, LEN = 102, FIND = 103, MATCH = 104, REVERSE = 105, SUB = 106, HASH = 107, BYTE = 108, CHAR = 109, TRIM = 110, SPLIT = 111, UUID = 112, GETCMD = 113, GETARGS = 114, ENV = 115;
     public static final int TB_INSERT = 200, TB_CONCAT = 201, TB_REMOVE = 202, TB_SORT = 203, TB_MOVE = 204, TB_UNPACK = 205, TB_PACK = 206, TB_DECODE = 207;
-    public static final int EXEC = 300, GETENV = 301, SETENV = 302, CLOCK = 303, SETLOC = 304, EXIT = 305, DATE = 306, GETPID = 307, SETPROC = 308, GETPROC = 309, GETCWD = 310, GETUID = 311, CHDIR = 312, REQUEST = 313, START = 314, STOP = 315, PREQ = 316;
+    public static final int EXEC = 300, GETENV = 301, SETENV = 302, CLOCK = 303, SETLOC = 304, EXIT = 305, DATE = 306, GETPID = 307, SETPROC = 308, GETPROC = 309, GETCWD = 310, GETUID = 311, CHDIR = 312, REQUEST = 313, START = 314, STOP = 315, PREQ = 316, PROCESS = 317;
     public static final int READ = 400, WRITE = 401, CLOSE = 402, OPEN = 403, POPEN = 404, DIRS = 405, SETOUT = 406, MOUNT = 407;
     public static final int HTTP_GET = 500, HTTP_POST = 501, CONNECT = 502, PEER = 503, DEVICE = 504, SERVER = 505, ACCEPT = 506;
     public static final int DISPLAY = 600, NEW = 601, RENDER = 602, APPEND = 603, ADDCMD = 604, HANDLER = 605, GETCURRENT = 606, TITLE = 607, TICKER = 608, VIBRATE = 609, LABEL = 610, SETTEXT = 611, GETLABEL = 612, GETTEXT = 613;
@@ -1431,12 +1431,22 @@ public class Lua {
                     else if (target.startsWith("/mnt/")) {
                         FileConnection fc = (FileConnection) Connector.open("file:///" + target.substring(5), Connector.READ); 
                         boolean exist = fc.exists(), dir = fc.isDirectory();
-                        fc.close(); if (exist && dir) { father.put("PWD", target); return new Double(0); } 
+                        fc.close(); 
+                        if (exist && dir) { father.put("PWD", target); return new Double(0); } 
                         else { return new Double(exist ? 5 : 127); }
                     }
 
                     return new Double(127);
                 }
+            }
+            else if (MOD == PROCESS) {
+                Hashtable result = new Hashtable();
+                for (Enumeration procs = midlet.sys.keys(); procs.hasMoreElements();) {
+                    String pid = (String) procs.nextElement();
+
+                    result.put(pid, midlet.getobject(pid, "name"));
+                }
+                return result;
             }
             // Package: io
             else if (MOD == READ) {
