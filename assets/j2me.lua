6a881e53af174dce508958e0b02bf35d7826c3da
Mr Lima <felipebr4095@gmail.com>
2025-11-26 21:54:47 -0300

    J2ME Lua API

--- assets/j2me.lua
+++ assets/j2me.lua
@@ -21,8 +21,9 @@ io = {
     read = function (file, lenght) end,
     write = function (text, file, mode) return 0 end,
     close = function (...) end,
-    popen = function (command, sudo) return 0, "" end,
-    dirs = function (pwd) return {} end
+    popen = function (program, argument, sudo, out, scope) return 0, "" end,
+    dirs = function (pwd) return {} end,
+    mount = function (struct) end
 }
 
 table = {
