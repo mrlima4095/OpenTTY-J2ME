12d2f4351ee22330d2c874115c771b935f3ceb70
Mr Lima <felipebr4095@gmail.com>
2025-11-29 15:43:20 -0300

    midlet

--- src/Lua.java
+++ src/Lua.java
@@ -2308,7 +2308,7 @@ public class Lua {
                 else {
                     try {
                         InputStream is = midlet.getInputStream(toLuaString(args.elementAt(0)));
-                        ELF elf = new ELF(this); 
+                        ELF elf = new ELF(midlet); 
                         if (elf.load(is)) {
                             elf.run();
                         } else {
