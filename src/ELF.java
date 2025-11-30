ca934646b8b79138c37df53b5d1518f770da1978
Mr Lima <felipebr4095@gmail.com>
2025-11-30 15:58:39 -0300

    ELF 32 - v2

--- src/ELF.java
+++ src/ELF.java
@@ -402,34 +402,364 @@ import java.io.*;
         }
     }
 }*/
-
 public class ELF {
     private OpenTTY midlet;
     private Object stdout;
     
+    // Registradores da CPU
+    private int[] registers;
+    private int eip;
+    private int eflags;
+    
+    // Memória 
+    private byte[] memory;
+    private static final int MEMORY_SIZE = 4 * 1024 * 1024; // 4MB
+    
+    // Constantes dos registradores
+    private static final int EAX = 0;
+    private static final int EBX = 1;
+    private static final int ECX = 2;
+    private static final int EDX = 3;
+    private static final int ESI = 4;
+    private static final int EDI = 5;
+    private static final int EBP = 6;
+    private static final int ESP = 7;
+    
+    // Syscalls Linux
+    private static final int SYS_EXIT = 1;
+    private static final int SYS_READ = 3;
+    private static final int SYS_WRITE = 4;
+    private static final int SYS_OPEN = 5;
+    private static final int SYS_CLOSE = 6;
+    
     public ELF(OpenTTY midlet, Object stdout) {
         this.midlet = midlet;
         this.stdout = stdout;
+        this.registers = new int[8];
+        this.memory = new byte[MEMORY_SIZE];
+        reset();
+    }
+    
+    private void reset() {
+        for (int i = 0; i < registers.length; i++) {
+            registers[i] = 0;
+        }
+        eip = 0;
+        eflags = 0;
+        
+        // Inicializar memória
+        for (int i = 0; i < MEMORY_SIZE; i++) {
+            memory[i] = 0;
+        }
+        
+        // Stack pointer no topo da memória
+        registers[ESP] = MEMORY_SIZE - 4;
+        registers[EBP] = MEMORY_SIZE - 4;
     }
     
     public boolean load(InputStream is) {
         try {
-            // Apenas verifica se é um ELF válido
+            midlet.print("Carregando ELF...", stdout);
+            
+            // Ler magic number
             byte[] magic = new byte[4];
-            int read = 0;
-            int b;
-            while (read < 4 && (b = is.read()) != -1) {
-                magic[read++] = (byte)b;
+            if (readBytes(is, magic, 4) != 4) {
+                return false;
             }
             
-            return (magic[0] == 0x7F && magic[1] == 'E' && magic[2] == 'L' && magic[3] == 'F');
+            // Verificar assinatura ELF
+            if (magic[0] != 0x7F || magic[1] != 'E' || magic[2] != 'L' || magic[3] != 'F') {
+                return false;
+            }
+            
+            midlet.print("ELF válido encontrado", stdout);
+            
+            // Ler o resto do header ELF (52 bytes total)
+            byte[] header = new byte[48];
+            if (readBytes(is, header, 48) != 48) {
+                return false;
+            }
+            
+            // Extrair informações do header
+            int entryPoint = readInt32(header, 24 - 4); // Entry point está no offset 24
+            int phoff = readInt32(header, 28 - 4);     // Program header offset
+            int phentsize = readInt16(header, 42 - 4); // Tamanho do program header
+            int phnum = readInt16(header, 44 - 4);     // Número de program headers
+            
+            midlet.print("Entry point: 0x" + Integer.toHexString(entryPoint), stdout);
+            midlet.print("Program headers: " + phnum, stdout);
+            
+            // Para simplificar, vamos carregar o arquivo inteiro na memória a partir do offset 0
+            is.close();
+            is = midlet.getInputStream("/bin/hello"); // Reabrir
+            
+            int totalLoaded = 0;
+            int byteRead;
+            while ((byteRead = is.read()) != -1 && totalLoaded < MEMORY_SIZE) {
+                memory[totalLoaded] = (byte) byteRead;
+                totalLoaded++;
+            }
+            
+            midlet.print("Bytes carregados: " + totalLoaded, stdout);
+            
+            // Configurar entry point
+            eip = entryPoint;
+            
+            // Inicializar stack
+            registers[ESP] = MEMORY_SIZE - 64;
+            registers[EBP] = MEMORY_SIZE - 64;
+            
+            return true;
+            
         } catch (Exception e) {
+            midlet.print("Erro no carregamento: " + e.toString(), stdout);
             return false;
         }
     }
     
     public void run() {
-        // Simplesmente imprime Hello World
-        midlet.print("Hello World from ELF emulator!", stdout);
+        midlet.print("Executando programa...", stdout);
+        
+        try {
+            int instructionCount = 0;
+            int maxInstructions = 1000; // Limite de segurança
+            
+            while (eip < MEMORY_SIZE && eip >= 0 && instructionCount < maxInstructions) {
+                if (eip >= MEMORY_SIZE - 1) {
+                    midlet.print("EIP fora da memória", stdout);
+                    break;
+                }
+                
+                int opcode = memory[eip] & 0xFF;
+                instructionCount++;
+                
+                // Decodificar instrução
+                switch (opcode) {
+                    case 0xCD: // INT - System call
+                        if (memory[eip + 1] == (byte)0x80) {
+                            handleSyscall();
+                            eip += 2;
+                        } else {
+                            eip++;
+                        }
+                        break;
+                        
+                    case 0xC3: // RET
+                        eip = pop32();
+                        break;
+                        
+                    case 0x90: // NOP
+                        eip++;
+                        break;
+                        
+                    case 0x50: // PUSH EAX
+                        push32(registers[EAX]);
+                        eip++;
+                        break;
+                        
+                    case 0x53: // PUSH EBX
+                        push32(registers[EBX]);
+                        eip++;
+                        break;
+                        
+                    case 0x51: // PUSH ECX
+                        push32(registers[ECX]);
+                        eip++;
+                        break;
+                        
+                    case 0x52: // PUSH EDX
+                        push32(registers[EDX]);
+                        eip++;
+                        break;
+                        
+                    case 0x58: // POP EAX
+                        registers[EAX] = pop32();
+                        eip++;
+                        break;
+                        
+                    case 0x5B: // POP EBX
+                        registers[EBX] = pop32();
+                        eip++;
+                        break;
+                        
+                    case 0x59: // POP ECX
+                        registers[ECX] = pop32();
+                        eip++;
+                        break;
+                        
+                    case 0x5A: // POP EDX
+                        registers[EDX] = pop32();
+                        eip++;
+                        break;
+                        
+                    case 0xB8: // MOV EAX, immediate32
+                        registers[EAX] = read32(eip + 1);
+                        eip += 5;
+                        break;
+                        
+                    case 0xBB: // MOV EBX, immediate32
+                        registers[EBX] = read32(eip + 1);
+                        eip += 5;
+                        break;
+                        
+                    case 0xB9: // MOV ECX, immediate32
+                        registers[ECX] = read32(eip + 1);
+                        eip += 5;
+                        break;
+                        
+                    case 0xBA: // MOV EDX, immediate32
+                        registers[EDX] = read32(eip + 1);
+                        eip += 5;
+                        break;
+                        
+                    case 0xEB: // JMP rel8
+                        byte rel8 = memory[eip + 1];
+                        eip += rel8 + 2;
+                        break;
+                        
+                    case 0x74: // JE rel8
+                        if ((eflags & 0x40) != 0) { // Zero flag
+                            byte rel8_je = memory[eip + 1];
+                            eip += rel8_je + 2;
+                        } else {
+                            eip += 2;
+                        }
+                        break;
+                        
+                    case 0x75: // JNE rel8
+                        if ((eflags & 0x40) == 0) { // Not zero flag
+                            byte rel8_jne = memory[eip + 1];
+                            eip += rel8_jne + 2;
+                        } else {
+                            eip += 2;
+                        }
+                        break;
+                        
+                    default:
+                        // Instrução não implementada - pular
+                        eip++;
+                        break;
+                }
+                
+                // Verificar se programa terminou
+                if (eip == 0 || instructionCount >= maxInstructions) {
+                    break;
+                }
+            }
+            
+            if (instructionCount >= maxInstructions) {
+                midlet.print("Limite de instruções atingido", stdout);
+            }
+            
+            midlet.print("Execução finalizada após " + instructionCount + " instruções", stdout);
+            
+        } catch (Exception e) {
+            midlet.print("Erro na execução: " + e.toString(), stdout);
+        }
+    }
+    
+    private void handleSyscall() {
+        int syscall = registers[EAX];
+        
+        switch (syscall) {
+            case SYS_EXIT:
+                midlet.print("Programa finalizado com código: " + registers[EBX], stdout);
+                eip = MEMORY_SIZE; // Forçar término
+                break;
+                
+            case SYS_WRITE:
+                int fd = registers[EBX];
+                int buf = registers[ECX];
+                int count = registers[EDX];
+                
+                if (fd == 1 || fd == 2) { // stdout ou stderr
+                    StringBuffer output = new StringBuffer();
+                    for (int i = 0; i < count; i++) {
+                        if (buf + i < MEMORY_SIZE) {
+                            output.append((char) memory[buf + i]);
+                        }
+                    }
+                    midlet.print(output.toString(), stdout);
+                    registers[EAX] = count; // Retornar número de bytes escritos
+                } else {
+                    registers[EAX] = -1; // Erro
+                }
+                break;
+                
+            case SYS_READ:
+                // Por enquanto, retornar 0 bytes lidos
+                registers[EAX] = 0;
+                break;
+                
+            case SYS_OPEN:
+                // Por enquanto, retornar erro
+                registers[EAX] = -1;
+                break;
+                
+            case SYS_CLOSE:
+                // Por enquanto, sempre sucesso
+                registers[EAX] = 0;
+                break;
+                
+            default:
+                midlet.print("Syscall não implementada: " + syscall, stdout);
+                registers[EAX] = -1;
+                break;
+        }
+    }
+    
+    // Métodos auxiliares
+    private int readBytes(InputStream is, byte[] buffer, int count) {
+        try {
+            int total = 0;
+            while (total < count) {
+                int b = is.read();
+                if (b == -1) break;
+                buffer[total] = (byte) b;
+                total++;
+            }
+            return total;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+    
+    private int readInt32(byte[] data, int offset) {
+        return (data[offset] & 0xFF) | 
+               ((data[offset + 1] & 0xFF) << 8) | 
+               ((data[offset + 2] & 0xFF) << 16) | 
+               ((data[offset + 3] & 0xFF) << 24);
+    }
+    
+    private int readInt16(byte[] data, int offset) {
+        return (data[offset] & 0xFF) | ((data[offset + 1] & 0xFF) << 8);
+    }
+    
+    private int read32(int addr) {
+        if (addr + 3 >= MEMORY_SIZE) return 0;
+        return (memory[addr] & 0xFF) | 
+               ((memory[addr + 1] & 0xFF) << 8) | 
+               ((memory[addr + 2] & 0xFF) << 16) | 
+               ((memory[addr + 3] & 0xFF) << 24);
+    }
+    
+    private void push32(int value) {
+        registers[ESP] -= 4;
+        write32(registers[ESP], value);
+    }
+    
+    private int pop32() {
+        int value = read32(registers[ESP]);
+        registers[ESP] += 4;
+        return value;
+    }
+    
+    private void write32(int addr, int value) {
+        if (addr + 3 < MEMORY_SIZE) {
+            memory[addr] = (byte) (value & 0xFF);
+            memory[addr + 1] = (byte) ((value >> 8) & 0xFF);
+            memory[addr + 2] = (byte) ((value >> 16) & 0xFF);
+            memory[addr + 3] = (byte) ((value >> 24) & 0xFF);
+        }
     }
 }
