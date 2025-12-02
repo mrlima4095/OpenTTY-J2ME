c5441400f4d2e2d779b6b69a853e9c6a5f623ef7
Mr Lima <felipebr4095@gmail.com>
2025-12-01 21:24:56 -0300

    ELF(OpenTTY midlet, Object stdout, Hashtable scope, Integer id) { ... }

--- src/Lua.java
+++ src/Lua.java
@@ -2315,7 +2315,7 @@ public class Lua {
                         
                         if (is == null) { return new Double(127); }
                         
-                        ELF elf = new ELF(midlet, stdout);
+                        ELF elf = new ELF(midlet, stdout, scope, id);
                         
                         if (elf.load(is)) { elf.run(); } 
                         else { return new Double(1); }
