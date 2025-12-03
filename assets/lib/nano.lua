6e5bbd371771212aab4752e0a38f826fa5311305
Mr Lima <felipebr4095@gmail.com>
2025-12-02 21:46:47 -0300

    /bin/nano

--- assets/lib/nano.lua
+++ assets/lib/nano.lua
@@ -6,48 +6,27 @@ local app = {
     clear = graphics.new("command", { label = "Clear", type = "screen", priority = 1 })
 }
 
-os.setproc("name", "nano")
-
-local function joinpath(pwd)
-    if string.sub(pwd, 1, 1) ~= "/" then
-        return os.getcwd() .. pwd
-    end
-    return pwd
-end
+local libcore = require("libcore")
 
 
 if arg[1] then
-    app.content = io.read(joinpath(arg[1]))
+    app.content = io.read(libcore.joinpath(arg[1]))
     app.editor = graphics.new("edit", "Nano - " .. arg[1], app.content)
 else
-    print("nano [file]") os.exit(2)
+    print("nano: usage: nano [file]")
+    os.exit(2)
 end
-
-app.save = function ()
-    local status = io.write(app.modified, joinpath(arg[1]))
-    if status == 0 then
-        return true
-    end
-
-    local message = "java.io.IOException"
-    if status == 5 then message = "read-only storage" elseif status == 13 then message = "Permission denied!" end
-
-    graphics.display(graphics.new("alert", "Error", message), app.editor)
-    return false
-end
-
-
 
 graphics.addCommand(app.editor, app.back)
 graphics.addCommand(app.editor, app.clear)
 graphics.handler(app.editor, {
-    [app.back] = function(content)
-        io.write(content, joinpath(arg[1]))
+    [app.back] = function (content)
+        local status = io.write(content, libcore.joinpath(arg[1]))
         graphics.display(previous)
+        os.exit(tonumber(status))
     end,
-    [app.clear] = function()
+    [app.clear] = function ()
         graphics.SetText(app.editor, "")
     end
 })
-
 graphics.display(app.editor)
