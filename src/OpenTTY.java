ab9641ce001d6d67484c935ddf42a9b4a85f6033
Lima <felipebr4095@gmail.com>
2025-12-22 07:01:59 -0300

    update OpenTTY.java

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -101,7 +101,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
                     warn("Login", "Invalid user name!");
                 } 
                 else {
-                    writeRMS("OpenRMS", user.getBytes(), 1); username = value;
+                    writeRMS("OpenRMS", user.getBytes(), 1); username = user;
                     writeRMS("OpenRMS", String.valueOf(password.hashCode()).getBytes(), 2);
                     init();
                 }
