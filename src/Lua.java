ac01fd8e96ed49126afad0c5e9e653236c99c34d
Mr Lima <felipebr4095@gmail.com>
2025-12-28 13:14:24 -0300

    Service 1 (Kernel) - Controls cache usage

--- src/Lua.java
+++ src/Lua.java
@@ -2365,6 +2365,18 @@ public class Lua {
                         else if (arg instanceof LuaFunction) { midlet.shell = arg; }
                         else { return new Double(2); }
                     }
+                    else if (payload.equals("cache")) { 
+                        if (arg == null || arg.equals("")) { return new Boolean(midlet.useCache); }
+                        else if (arg == Boolean.TRUE || toLuaString(arg).equals("true")) {
+                            midlet.useCache = true;
+                        }
+                        else if (arg == Boolean.FALSE || toLuaString(arg).equals("false")) {
+                            midlet.useCache = false;
+                            midlet.cache.clear();
+                            midlet.cacheLua.clear();
+                        }
+                        else { return new Double(2); }
+                    }
                 }
             }
 
