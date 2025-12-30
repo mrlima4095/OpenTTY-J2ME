b294166c5e8dccdef8985353b4674c7abe85dc55
Mr Lima <felipebr4095@gmail.com>
2025-12-30 14:31:00 -0300

    Object

--- src/ELF.java
+++ src/ELF.java
@@ -1,9 +1,9 @@
 public class ELF {
-    public ELF(OpenTTY midlet, Object stdout, Hashtable scope, int id, String pid, Hashtable proc) { }
+    public ELF(OpenTTY midlet, Object stdout, Object scope, int id, String pid, Object proc) { }
     public String getPid() { return "-1"; }
     public void kill() { }
     
-    public boolean load(InputStream is) throws Exception { }
+    public boolean load(Object is) throws Exception { }
 
     public void run() { }
 }
