453e734eaeabbef3653b9a1bbd8f8141ae0676cd
Mr Lima <felipebr4095@gmail.com>
2025-12-30 14:29:59 -0300

    ELF toy

--- ELF.java
+++
@@ -1,9 +0,0 @@
-public class ELF {
-    public ELF(OpenTTY midlet, Object stdout, Hashtable scope, int id, String pid, Hashtable proc) { }
-    public String getPid() { return "-1"; }
-    public void kill() { }
-    
-    public boolean load(InputStream is) throws Exception { }
-
-    public void run() { }
-}
