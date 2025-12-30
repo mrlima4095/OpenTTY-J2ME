d5597d29487e79e7b1d31d1dd8519a95993b37fc
Mr Lima <felipebr4095@gmail.com>
2025-12-30 14:38:57 -0300

    ELF bit

---
+++ ELF.java
@@ -0,0 +1,9 @@
+public class ELF {
+    public ELF(OpenTTY midlet, Object stdout, Object scope, int id, String pid, Object proc) { }
+    public String getPid() { return "-1"; }
+    public void kill() { }
+    
+    public boolean load(Object is) throws Exception { return false; }
+
+    public void run() { }
+}
