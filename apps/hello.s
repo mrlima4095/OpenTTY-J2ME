c0351f24b3f2ec5d9b0f1ac3bec33e70c0f59e02
Mr Lima <felipebr4095@gmail.com>
2025-12-31 10:06:47 -0300

    hello

--- apps/hello.s
+++ apps/hello.s
@@ -1,19 +1,30 @@
-/* hello_emulator.s - Específico para seu emulador */
+/* hello_clang_asm.s - Assembly otimizado para Clang ARM */
+.syntax unified
+.cpu arm7tdmi
+.arch armv4t
+.fpu softvfp
+
 .global _start
 .align 2
+.section .text,"ax",%progbits
 
 _start:
-    /* write(1, "Hello", 5) */
-    mov r0, #1            @ stdout
-    adr r1, msg           @ endereço da mensagem
-    mov r2, #5            @ tamanho
-    mov r7, #4            @ syscall write
-    swi #0                @ interrupção
+    /* write(1, message, length) */
+    mov r0, #1          @ fd = stdout
+    ldr r1, =message    @ buffer
+    mov r2, #18         @ length
+    mov r7, #4          @ syscall write
+    swi #0              @ syscall
     
     /* exit(0) */
-    mov r0, #0            @ código de saída
-    mov r7, #1            @ syscall exit
-    swi #0
+    mov r0, #0          @ exit code
+    mov r7, #1          @ syscall exit
+    swi #0              @ syscall
+    
+    /* Se falhar, hang */
+1:  b 1b
 
-msg:
-    .ascii "Hello"
+.section .rodata,"a",%progbits
+.align 2
+message:
+    .asciz "Hello Clang ARM!\n"
