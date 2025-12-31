a3ff01572711c6564c5c2e1b50bef1100c52bfb3
Mr Lima <felipebr4095@gmail.com>
2025-12-31 19:12:06 -0300

    , stdout, id

--- src/ELF.java
+++ src/ELF.java
@@ -109,7 +109,7 @@ public class ELF {
         return load(baos.toByteArray());
     }
     public boolean load(byte[] elfData) throws Exception {
-        midlet.print("CHEGOU AQUI!");
+        midlet.print("CHEGOU AQUI!", stdout, id);
         if (elfData.length < 4 || elfData[0] != 0x7F || elfData[1] != 'E' || elfData[2] != 'L' || elfData[3] != 'F') { midlet.print("Not a valid ELF file", stdout); return false; }
         if (elfData[4] != ELFCLASS32) { midlet.print("Only 32-bit ELF supported", stdout); return false; }
         if (elfData[5] != ELFDATA2LSB) { midlet.print("Only little-endian ELF supported", stdout); return false; }
@@ -148,7 +148,7 @@ public class ELF {
     
     public Hashtable run() {
         running = true;
-        midlet.print("MANDANDO RODAR!");
+        midlet.print("MANDANDO RODAR!", stdout, id);
         
         Hashtable proc = midlet.genprocess("elf", id, null), ITEM = new Hashtable();
         proc.put("elf", this); midlet.sys.put(pid, proc);
