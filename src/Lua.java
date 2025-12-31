6a864d549ac67a27645f525cd449c582644e200c
Mr Lima <felipebr4095@gmail.com>
2025-12-31 14:11:38 -0300

    J2ME Lua API

--- src/Lua.java
+++ src/Lua.java
@@ -2015,7 +2015,7 @@ public class Lua {
 
                     SocketConnection conn = (SocketConnection) Connector.open(toLuaString(args.elementAt(0)));
                         
-                    result.addElement(conn); result.addElement(conn.openInputStream()); result.addElement(conn.openOutputStream());
+                    result.addElement(conn); result.addElement(conn.openInputStream()); result.addElement(conn.openOutputStream()); result.addElement(args.elementAt(0)); result.addElement(new Double(id));
                     midlet.network.put(conn, result);
 
                     return result;
@@ -2038,7 +2038,10 @@ public class Lua {
                 if (args.isEmpty() || !(args.elementAt(0) instanceof Double)) { return gotbad(1, "server" , "number expected, got " + (args.isEmpty() ? "no value" : type(args.elementAt(0)))); }
                 else {
                     ServerSocketConnection server = (ServerSocketConnection) Connector.open("socket://:" + toLuaString(args.elementAt(0)));
-                    midlet.network.put(server, args.elementAt(0));
+                    Vector result = new Vector();
+                    result.addElement(args.elementAt(0));
+                    result.addElement(new Double(id));
+                    midlet.network.put(server, result);
                     return server;
                 }
             }
