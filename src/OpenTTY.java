2daa9ab71c53d9dc7eaab446fd2a4ac52d573fe7
Mr Lima <felipebr4095@gmail.com>
2026-01-01 11:56:37 -0300

    Updating yearly messages

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -25,7 +25,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
     public TextBox nano = new TextBox("Nano", "", 31522, TextField.ANY);
     public Form form = new Form("OpenTTY " + getAppProperty("MIDlet-Version"));
     public TextField stdin = new TextField("Command", "", 256, TextField.ANY);
-    public StringItem stdout = new StringItem("", "Welcome to OpenTTY " + getAppProperty("MIDlet-Version") + "\nCopyright (C) 2025 - Mr. Lima\n");
+    public StringItem stdout = new StringItem("", "Welcome to OpenTTY " + getAppProperty("MIDlet-Version") + "\nCopyright (C) 2026 - Mr. Lima\n");
     private Command EXECUTE = new Command("Send", Command.OK, 0), HELP = new Command("Help", Command.SCREEN, 1), NANO = new Command("Nano", Command.SCREEN, 2), CLEAR = new Command("Clear", Command.SCREEN, 3), HISTORY = new Command("History", Command.SCREEN, 4),
                     BACK = new Command("Back", Command.BACK, 1), RUNS = new Command("Run Script", Command.OK, 1), IMPORT = new Command("Import File", Command.OK, 1), VIEW = new Command("View as HTML", Command.OK, 1);
     // |
@@ -125,7 +125,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
             monitor = new Form(form.getTitle());
 
             if (MOD == SIGNUP) {
-                monitor.append(env("Welcome to OpenTTY $VERSION\nCopyright (C) 2025 - Mr. Lima\n\n" + (asking_user && asking_passwd ? "Create your credentials!" : asking_user ? "Create an user to access OpenTTY!" : asking_passwd ? "Create a password!" : "")).trim());
+                monitor.append(env("Welcome to OpenTTY $VERSION\nCopyright (C) 2026 - Mr. Lima\n\n" + (asking_user && asking_passwd ? "Create your credentials!" : asking_user ? "Create an user to access OpenTTY!" : asking_passwd ? "Create a password!" : "")).trim());
 
                 if (asking_user) { monitor.append(USER = new TextField("Username", "", 256, TextField.ANY)); }
                 if (asking_passwd) { monitor.append(PASSWD = new TextField("Password", "", 256, TextField.ANY | TextField.PASSWORD)); }
@@ -1644,7 +1644,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
     // API 015 - (Scripts)
     // |
     // OpenTTY Packages
-    private void about(String script) { if (script == null || script.length() == 0) { warnCommand("About", env("OpenTTY $VERSION\n(C) 2025 - Mr. Lima")); return; } Hashtable PKG = parseProperties(getcontent(script)); if (PKG.containsKey("name")) { echoCommand((String) PKG.get("name") + " " + (String) PKG.get("version")); } if (PKG.containsKey("description")) { echoCommand((String) PKG.get("description")); } }
+    private void about(String script) { if (script == null || script.length() == 0) { warnCommand("About", env("OpenTTY $VERSION\n(C) 2026 - Mr. Lima")); return; } Hashtable PKG = parseProperties(getcontent(script)); if (PKG.containsKey("name")) { echoCommand((String) PKG.get("name") + " " + (String) PKG.get("version")); } if (PKG.containsKey("description")) { echoCommand((String) PKG.get("description")); } }
     private int importScript(String script) { return importScript(script, username.equals("root") ? true : false); }
     private int importScript(String script, boolean root) {
         if (script == null || script.length() == 0) { return 2; }
