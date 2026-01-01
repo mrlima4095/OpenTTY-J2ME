80028abdfbbcac83f5d6d3c9a777b84f96d1cf74
Mr Lima <felipebr4095@gmail.com>
2026-01-01 14:24:50 -0300

    /bin/nano

--- assets/lib/nano.lua
+++ assets/lib/nano.lua
@@ -4,8 +4,7 @@ local previous = graphics.getCurrent()
 local app = {
     back = graphics.new("command", { label = "Back", type = "back", priority = 1 }),
     clear = graphics.new("command", { label = "Clear", type = "screen", priority = 1 }),
-    yes = graphics.new("command", { label = "Yes", type = "ok", priority = 1 }),
-    no = graphics.new("command", { label = "No", type = "back", priority = 1 })
+    save = graphics.new("command", { label = "Save", type = "screen", priority = 1 })
 }
 
 if arg[1] then
@@ -21,33 +20,24 @@ function app.quit()
     graphics.display(previous)
     os.exit(0)
 end
-function app.save(content)
-    local alert = graphics.new("alert", "Nano", "Save Buffer?")
-    graphics.addCommand(alert, app.yes)
-    graphics.addCommand(alert, app.no)
-    graphics.handler(alert, {
-        [app.yes] = function ()
-            local status = io.write(content, os.join(arg[1]))
-            local message
+function app.write(content)
+    local status = io.write(content, os.join(arg[1]))
+    local message
 
-            if status == 1 then message = "java.io.IOException" elseif status == 5 then message = "read-only storage" elseif status == 13 then message = "permission denied" end
-            if status > 0 then
-                graphics.display(previous)
-                graphics.display(graphics.new("alert", "Nano", message))
-                os.exit(status)
-            end
+    if status == 1 then message = "java.io.IOException" elseif status == 5 then message = "read-only storage" elseif status == 13 then message = "permission denied" end
+    if status > 0 then
+        graphics.display(previous)
+        graphics.display(graphics.new("alert", "Nano", message))
+        os.exit(status)
+    end
 
-            graphics.display(previous)
-            os.exit(tonumber(status))
-        end,
-        [app.no] = app.quit()
-    })
-    graphics.display(alert)
+    graphics.display(previous)
+    os.exit(tonumber(status))
 end
 
 os.setproc("name", "nano")
 
 graphics.addCommand(app.editor, app.back)
 graphics.addCommand(app.editor, app.clear)
-graphics.handler(app.editor, { [app.back] = app.save, [app.clear] = function () graphics.SetText(app.editor, "") end })
+graphics.handler(app.editor, { [app.back] = app.quit, [app.clear] = function () graphics.SetText(app.editor, "") end, [app.save] = app.write })
 graphics.display(app.editor)
