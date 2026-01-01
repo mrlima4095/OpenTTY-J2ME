ce85ec6d77a750cdea83a714212505170dc4cafd
Mr Lima <felipebr4095@gmail.com>
2026-01-01 18:19:02 -0300

    ELF - Archive

--- assets/archive/ELF.java
+++ assets/archive/ELF.java
@@ -1,10 +1,16 @@
 public class ELF {
-    public ELF(OpenTTY midlet, Object stdout, Object scope, int id, String pid, Object proc) { }
-    public String getPid() { return "-1"; }
-    public void kill() { }
+    public ELF(OpenTTY midlet, Object stdout, Hashtable scope, int id, String pid, Hashtable proc) { }
     
-    public boolean load(Object is) throws Exception { return false; }
-    public boolean load(byte[] is) throws Exception { return false; }
+    public String getPid() { return ""; }
+    public void kill() {  }
+    
+    public boolean load(InputStream is) throws Exception { return false; }
+    public boolean load(byte[] elfData) throws Exception { return false; }
+    
+    public Hashtable run() {
+        Hashtable ITEM = new Hashtable();
 
-    public Hashtable run() { }
+        ITEM.put("status", new Double(1));
+        return ITEM;
+    }
 }
