eb1eb0a3584c1d70f21b99a5d5109bf4c061217d
Mr Lima <felipebr4095@gmail.com>
2026-01-01 14:19:00 -0300

    Code wrap

--- assets/lib/netcat.lua
+++ assets/lib/netcat.lua
@@ -22,14 +22,7 @@ if arg[1] and arg[2] then
     local run = graphics.new("command", { label = "Send", type = "ok", priority = 1 })
     local buffer = graphics.new("buffer", { })
 
-    java.run(function()
-        while running do
-            local x, response = pcall(io.read, i)
-            if x then
-                io.write(response, buffer, "a")
-            end
-        end
-    end, "Background")
+    java.run(function() while running do local x, response = pcall(io.read, i) if x then io.write(response, buffer, "a") end end end, "Background")
 
     graphics.append(screen, buffer)
     graphics.append(screen, { type = "field", label = "Remote (" .. remote .. ")", })
