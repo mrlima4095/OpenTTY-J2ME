05a02693222fd5c0c23a999df07c0968b000af65
Mr Lima <felipebr4095@gmail.com>
2026-01-02 12:16:53 -0300

    debugging v2

--- apps/ls.s
+++ apps/ls.s
@@ -1,19 +1,51 @@
-@ minimal.s - Teste absolutamente mínimo
+@ test_open.s - Testa apenas open()
 .section .data
-msg: .asciz "HELLO\n"
+dir:    .asciz "/home/"
+msg_ok: .asciz "OPEN OK\n"
+msg_fail: .asciz "OPEN FAIL\n"
 
 .section .text
 .global _start
 
 _start:
-    @ write(1, msg, 6)
-    mov r0, #1      @ stdout
-    ldr r1, =msg
-    mov r2, #6      @ tamanho
-    mov r7, #4      @ SYS_WRITE
+    @ Tentar abrir diretório
+    ldr r0, =dir
+    mov r1, #0x10000   @ O_DIRECTORY
+    mov r7, #5         @ SYS_OPEN
     svc 0
     
-    @ exit(0)
-    mov r0, #0
-    mov r7, #1      @ SYS_EXIT
+    @ Verificar resultado
+    cmp r0, #0
+    bgt open_success
+    
+open_fail:
+    @ Imprimir falha
+    ldr r1, =msg_fail
+    mov r2, #10
+    bl write
+    mov r0, #1         @ exit code 1
+    b exit
+
+open_success:
+    @ Imprimir sucesso
+    ldr r1, =msg_ok
+    mov r2, #9
+    bl write
+    
+    @ Fechar fd
+    mov r7, #6         @ SYS_CLOSE
     svc 0
+    
+    mov r0, #0         @ exit code 0
+
+exit:
+    mov r7, #1         @ SYS_EXIT
+    svc 0
+
+write:
+    push {r0, r7}
+    mov r0, #1
+    mov r7, #4
+    svc 0
+    pop {r0, r7}
+    bx lr
