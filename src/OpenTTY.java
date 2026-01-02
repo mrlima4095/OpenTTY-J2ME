01abe5e65339e2da20db643947fb76154876ec3c
Mr Lima <felipebr4095@gmail.com>
2026-01-01 21:46:51 -0300

    Build "2026-1.17.1-03x10" - 1.17.1 Suite case

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -21,7 +21,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
     public Object shell;
 
     public Hashtable attributes = new Hashtable(), fs = new Hashtable(), sys = new Hashtable(), tmp = new Hashtable(), cache = new Hashtable(), cacheLua = new Hashtable(), graphics = new Hashtable(), network = new Hashtable(), globals = new Hashtable();
-    public String username = read("/home/OpenRMS"), build = "2026-1.17-03x09";
+    public String username = read("/home/OpenRMS"), build = "2026-1.17.1-03x10";
     // |
     // Graphics
     public Display display = Display.getDisplay(this);
