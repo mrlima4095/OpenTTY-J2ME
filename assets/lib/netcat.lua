b1c4d4bde4e4fbe5ad99c2b2066b5b17afaa6988
Mr Lima <felipebr4095@gmail.com>
2026-01-04 12:19:25 -0300

    netcat

--- assets/lib/netcat.lua
+++ assets/lib/netcat.lua
@@ -6,19 +6,20 @@ os.setproc("name", "nc")
 
 if arg[1] and arg[2] then
     local remote, port = arg[1], arg[2]
-    local running = true
+    local running = false
 
     local ok, conn, i, o = pcall(socket.connect, "socket://" .. remote .. ":" .. port)
     if not ok then
         print("nc: " .. tostring(conn))
         os.exit(101)
+    else
+        running = true
     end
 
     local previous = graphics.getCurrent()
     local screen = graphics.new("screen", "OpenTTY " .. os.getenv("VERSION"))
     local back = graphics.new("command", { label = "Back", type = "screen", priority = 1 })
     local clear = graphics.new("command", { label = "Clear", type = "screen", priority = 1 })
-    local info = graphics.new("command", { label = "View info", type = "screen", priority = 1 })
     local run = graphics.new("command", { label = "Send", type = "ok", priority = 1 })
     local buffer = graphics.new("buffer", { })
 
@@ -30,7 +31,6 @@ if arg[1] and arg[2] then
     graphics.addCommand(screen, run)
     graphics.addCommand(screen, back)
     graphics.addCommand(screen, clear)
-    graphics.addCommand(screen, info)
     graphics.handler({
         [back] = function ()
             graphics.display(previous)
@@ -38,9 +38,10 @@ if arg[1] and arg[2] then
             os.exit(0)
         end,
         [clear] = function () graphics.SetText(buffer, "") end,
-        [info] = function () graphics.display(graphics.new("alert", "Informations", "")) end,
         [run] = function (payload) pcall(io.write, payload, o) end
     })
     graphics.display(screen)
-
+else
+    print("nc: usage: nc [host] [port]")
+    os.exit(2)
 end
