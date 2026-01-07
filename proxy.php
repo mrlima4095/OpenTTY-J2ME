2b9509490623757abebd679d6b584da79c3bbddb
Mr Lima <felipebr4095@gmail.com>
2026-01-07 16:33:09 -0300

    proxy.php

---
+++ proxy.php
@@ -0,0 +1,41 @@
+<?php
+$url = $_SERVER['QUERY_STRING'] ?? '';
+
+if (strpos($url, 'url=') === 0) { $url = substr($url, 4); $url = urldecode($url); }
+
+if (empty($url)) {
+    http_response_code(400);
+    echo "Error: No URL provided";
+    exit;
+}
+
+if (!preg_match('#^https?://#i', $url)) { $url = 'https://' . $url; }
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+if (isset($_SERVER['HTTP_USER_AGENT'])) { curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); }
+
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+if (curl_errno($ch)) {
+    http_response_code(502);
+    echo "" . curl_error($ch);
+    curl_close($ch);
+    exit;
+}
+
+curl_close($ch);
+
+
+if ($content_type) {
+    header("Content-Type: $content_type");
+}
+
+http_response_code($http_code);
+echo $response;
