ccf9c56ec33b589490117abab37a0c69329bf42d
Mr Lima <felipebr4095@gmail.com>
2026-01-07 16:28:25 -0300

    -- latest.sh

--- assets/scripts/latest.sh
+++
@@ -1,9 +0,0 @@
-#!/java/bin/sh
-# -*- coding: utf-8 -*-
-
-set ONLINE_VERSION=1.17
-
-
-if ($VERSION == $ONLINE_VERSION) echo already up to date.
-if ($VERSION != $ONLINE_VERSION) execute echo A new version was released!; echo; echo Local - OpenTTY $VERSION; echo Server - OpenTTY $ONLINE_VERSION; open http://opentty.xyz/dist/versions/OpenTTY-$ONLINE_VERSION.jar;
-
