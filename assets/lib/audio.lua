df4290d8c70368b0f7aa1a8a679428349e581806
Mr Lima <felipebr4095@gmail.com>
2026-01-06 21:42:12 -0300

    /bin/audio

--- assets/lib/audio.lua
+++ assets/lib/audio.lua
@@ -1,76 +1,112 @@
 #!/bin/lua
 
-os.setproc("name", "audio")
-
 if arg[1] == "--deamon" then
+    os.setproc("name", "audio-codec")
     local ok, status, player
 
     return function (payload, args, scope, pid, uid)
         if payload == "play" then
             ok, player = pcall(audio.load, args)
-            if not ok then
+            if ok then
+                ok, status = pcall(audio.play, player)
+                if ok then
+                    return ":: playing"
+                else
+                    pcall(io.close, player)
+                    player = nil
+                    return ":: failed to play"
+                end
+            else
                 return ":: loading failed - " .. tostring(player)
             end
-            ok, status = pcall(audio.play, player)
-            if not ok or status ~= 0 then
-                pcall(io.close, player)
-                player = nil
-
-                return ":: failed to play"
-            end
-        elseif payload == "pause" then
+        elseif player == "pause" then
             if player == nil then
-                return ":: no audio running"
+                return ": no running audio"
             end
 
             ok, status = pcall(audio.pause, player)
-            if not ok or status ~= 0 then
+            if ok and status == 0 then
+                return ":: paused"
+            else
                 return ":: failed to pause"
             end
-        elseif payload == "resume" then
+        elseif player == "resume" then
             if player == nil then
-                return ":: no audio running"
+                return ":: no running audio"
             end
 
             ok, status = pcall(audio.play, player)
-            if not ok or status ~= 0 then
-                return ":: failed to play"
+            if ok and status == 0 then
+                return ":: resumed"
+            else
+                return ":: failed to resume"
             end
-
-        elseif payload == "stop" then
+        elseif player == "stop" then
             if player == nil then
-                return ":: no audio running"
+                return ":: no running audio"
             end
 
-            ok, status = pcall(audio.pause, player)
+            ok, status = pcall(audio.play, player)
             ok, status = pcall(io.close, player)
-        elseif payload == "volume" then
+            player = nil
+            return ":: stopped"
+        elseif player == "volume" then
             if player == nil then
-                return ":: no audio running"
+                return ":: no running audio"
             end
 
-            ok, status = pcall(audio.play, player)
-            if not ok or status ~= 0 then
-                return ":: failed to play"
+            if args then
+                ok, status = pcall(audio.volume, player, tonumber(args))
+                if ok and status == 0 then
+                    return ":: volume set"
+                else
+                    return ":: failed to set volume"
+                end
+            else
+                ok, status = pcall(audio.volume, player)
+                if ok then
+                    return ":: " .. status
+                else
+                    return ":: failed to get volume"
+                end
             end
+        elseif player == "status" then
+            if player == nil then
+                return ":: no running audio"
+            else
+                local time, duration = audio.time(player), audio.duration(player)
+                if time and duration then
+                    return ":: playing (" .. time .. "/" .. duration .. " sec)"
+                else
+                    return ":: playing (unknown duration)"
+                end
+            end
+        else
+            return ":: unknown command"
         end
     end
 end
 
-local codec = os.getpid("audio")
+os.setproc("name", "audio-cli")
+
+local codec = os.getpid("audio-codec")
 if codec == nil then
     print(":: audio codec not running")
     print(":: loading...")
 
     os.request("1", "serve", arg[0])
-    codec = os.getpid("audio")
+    codec = os.getpid("audio-codec")
     print(":: codec running with pid " .. codec)
 end
 
 if arg[1] then
-    local message, status os.request(codec, arg[1], arg[2])
-    
-    print(message)
+    local message = os.request(codec, arg[1], arg[2])
 
-    os.exit(status)
+    if message then
+        print(message)
+    end
+
+    os.exit(0)
+else
+    print("audio [option] [file]")
 end
