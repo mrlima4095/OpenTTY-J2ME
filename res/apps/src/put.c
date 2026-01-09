cf8ba9eada8b4964cac89a4775888e3e65bdce49
Mr Lima <felipebr4095@gmail.com>
2026-01-09 16:13:14 -0300

    put put.c

---
+++ res/apps/src/put.c
@@ -0,0 +1,13 @@
+// test_putchar.c
+#include <stdio.h>
+
+int main() {
+    putchar('H');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar('!');
+    putchar('\n');
+    return 0;
+}
