9dd0fb3475f5fc7881365a863a4d682a94b94143
Mr Lima <felipebr4095@gmail.com>
2026-01-13 21:33:21 -0300

    uid

--- src/Process.java
+++ src/Process.java
@@ -13,7 +13,7 @@ public class Process {
     public ELF elf;
     
     public Process(OpenTTY midlet, String name, String owner, int uid, String pid, Object stdout, Hashtable scope) {
-        this.lua = new Lua(midlet, id, pid, this, stdout, scope);
+        this.lua = new Lua(midlet, uid, pid, this, stdout, scope);
         this.name = name; this.owner = owner; this.uid = uid; this.pid = pid;
         this.stdout = stdout; this.stderr = stdout; this.scope = scope;
         this.startTime = System.currentTimeMillis();
