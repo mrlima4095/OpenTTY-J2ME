aa87cff1473684d9950f1b9702ad4dfdc1cb4c8e
Mr Lima <felipebr4095@gmail.com>
2026-01-13 21:34:08 -0300

    new Hashtable()

--- src/Process.java
+++ src/Process.java
@@ -13,13 +13,13 @@ public class Process {
     public ELF elf;
     
     public Process(OpenTTY midlet, String name, String owner, int uid, String pid, Object stdout, Hashtable scope) {
-        this.lua = new Lua(midlet, uid, pid, this, stdout, scope);
+        this.lua = new Lua(midlet, uid, pid, new Hashtable(), stdout, scope);
         this.name = name; this.owner = owner; this.uid = uid; this.pid = pid;
         this.stdout = stdout; this.stderr = stdout; this.scope = scope;
         this.startTime = System.currentTimeMillis();
     }
     public Process(OpenTTY midlet, String name, int uid, String pid, Object stdout, Hashtable args, Hashtable scope) {
-        this.elf = new ELF(midlet, args, stdout, scope, uid, pid, this);
+        this.elf = new ELF(midlet, args, stdout, scope, uid, pid, new Hashtable());
         this.name = name; this.owner = owner; this.uid = uid; this.pid = pid;
         this.stdout = stdout; this.stderr = stdout; this.scope = scope;
         this.startTime = System.currentTimeMillis();
