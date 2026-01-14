d67607891212c9fd31282eb3159e99f3dd8977c2
Mr Lima <felipebr4095@gmail.com>
2026-01-14 13:38:39 -0300

    this Process;

--- src/Process.java
+++ src/Process.java
@@ -14,13 +14,14 @@ public class Process {
     public ELF elf;
     
     public Process(OpenTTY midlet, String name, String command, String owner, int uid, String pid, Object stdout, Hashtable scope) {
-        this.lua = new Lua(midlet, uid, pid, new Hashtable(), stdout, scope);
+        this.lua = new Lua(midlet, uid, pid, this, stdout, scope);
+        Lua(OpenTTY midlet, int id, String pid, Process proc, Object stdout, Hashtable scope)
         this.name = name; this.owner = owner; this.uid = uid; this.pid = pid;
         this.stdout = stdout; this.stderr = stdout; this.scope = scope;
         this.startTime = System.currentTimeMillis();
     }
     public Process(OpenTTY midlet, String name, String command, String owner, int uid, String pid, Object stdout, Hashtable args, Hashtable scope) {
-        this.elf = new ELF(midlet, args, stdout, scope, uid, pid, new Hashtable());
+        this.elf = new ELF(midlet, args, stdout, scope, uid, pid, this);
         this.name = name; this.owner = owner; this.uid = uid; this.pid = pid;
         this.stdout = stdout; this.stderr = stdout; this.scope = scope;
         this.startTime = System.currentTimeMillis();
