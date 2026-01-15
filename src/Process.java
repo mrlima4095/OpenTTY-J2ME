aed43d96df4b7f05ae5c3bc9bcc6f3b674eda390
Mr Lima <felipebr4095@gmail.com>
2026-01-14 22:09:29 -0300

    Process.toString();

--- src/Process.java
+++ src/Process.java
@@ -26,5 +26,5 @@ public class Process {
     }
 
 
-    public String toString() { return "{ name=" + name + ", owner=" + owner + ", uid=" + uid + ", pid=" + pid + ", " + (lua != null ? "lua=" + lua + ", " : elf != null ? "elf=" + elf + ", " : "") + (handler != null ? "handler=" + handler + ", " : "") + "priority=" + priority + ", scope=" + scope + ", db=" + db + "}"; }
+    public String toString() { return "{ name=" + name + ", owner=" + owner + ", uid=" + uid + ", pid=" + pid + ", " + (lua != null ? "lua=" + lua + ", " : elf != null ? "elf=" + elf + ", " : "") + (handler != null ? "handler=" + handler + ", " : "") + "priority=" + priority + ", scope=" + scope + ", db=" + db + " }"; }
 }
