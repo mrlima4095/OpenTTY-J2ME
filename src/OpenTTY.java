7ad6f5c4a111d39a7a8cabb681aef543a8dab981
Mr Lima <felipebr4095@gmail.com>
2026-01-14 21:27:18 -0300

    Process /bin/init - Kernel

--- src/OpenTTY.java
+++ src/OpenTTY.java
@@ -41,7 +41,7 @@ public class OpenTTY extends MIDlet implements CommandListener {
             globals.put("PWD", "/home/"); globals.put("USER", "root"); globals.put("ROOT", "/");
             Process proc = new Process(this, "init", "/bin/init", "root", 0, "1", stdout, globals);
 
-            sys.put("1", proc); proc.lua.globals.put("arg", args); proc.isService = true; proc.handler = proc.lua.getKernel();
+            sys.put("1", proc); proc.lua.globals.put("arg", args); proc.handler = proc.lua.getKernel();
             proc.lua.tokens = proc.lua.tokenize(read("/bin/init", globals)); 
 
             while (proc.lua.peek().type != 0) { Object res = proc.lua.statement(globals); if (proc.lua.doreturn) { break; } }
