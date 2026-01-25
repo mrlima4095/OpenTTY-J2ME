b63c4a1b2a0b954538601f7338764e75b72e7f03
Mr Lima <felipebr4095@gmail.com>
2026-01-25 15:26:18 -0300

    ELF Lite Archive

--- res/archive/ELF.java
+++ res/archive/ELF.java
@@ -7,6 +7,7 @@ import java.io.*;
 public class ELF {
     private Object stdout;
     private OpenTTY midlet;
+    private Hashtable scope;
 
     private static final int EI_NIDENT = 16;
     private static final int ELFCLASS32 = 1;
@@ -18,7 +19,7 @@ public class ELF {
     private static final int PT_INTERP = 3;
     private static final int PT_NOTE = 4;
 
-    public ELF(OpenTTY midlet, Hashtable args, Object stdout, Hashtable scope, int id, String pid, Hashtable proc) { this.midlet = midlet; this.stdout = stdout; }
+    public ELF(OpenTTY midlet, Hashtable args, Object stdout, Hashtable scope, int id, String pid, Process proc) { this.midlet = midlet; this.stdout = stdout; this.scope = scope; }
     
     public String getPid() { return ""; }
     public void kill() {  }
@@ -33,13 +34,13 @@ public class ELF {
     }
     public boolean load(byte[] elfData) throws Exception {
         if (elfData.length < 4 || elfData[0] != 0x7F || elfData[1] != 'E' || elfData[2] != 'L' || elfData[3] != 'F') { midlet.print("Not a valid ELF file", stdout); return false; }
-        if (elfData[4] != ELFCLASS32) { midlet.print("Only 32-bit ELF supported", stdout); return false; }
-        if (elfData[5] != ELFDATA2LSB) { midlet.print("Only little-endian ELF supported", stdout); return false; }
+        if (elfData[4] != ELFCLASS32) { midlet.print("Only 32-bit ELF supported", stdout, 0, scope); return false; }
+        if (elfData[5] != ELFDATA2LSB) { midlet.print("Only little-endian ELF supported", stdout, 0, scope); return false; }
         
         int e_type = readShortLE(elfData, 16), e_machine = readShortLE(elfData, 18), e_entry = readIntLE(elfData, 24), e_phoff = readIntLE(elfData, 28), e_shoff = readIntLE(elfData, 32), e_phnum = readShortLE(elfData, 44), e_shnum = readShortLE(elfData, 48), e_phentsize = readShortLE(elfData, 42), e_shentsize = readShortLE(elfData, 46);
         
-        if (e_type != ET_EXEC) { midlet.print("Not an executable ELF", stdout); return false; }
-        if (e_machine != EM_ARM) { midlet.print("Not an ARM executable", stdout); return false; }
+        if (e_type != ET_EXEC) { midlet.print("Not an executable ELF", stdout, 0, scope); return false; }
+        if (e_machine != EM_ARM) { midlet.print("Not an ARM executable", stdout, 0, scope); return false; }
         
         return true;
     }
@@ -47,7 +48,7 @@ public class ELF {
     public Hashtable run() {
         Hashtable ITEM = new Hashtable();
 
-        midlet.print("ELF not supported!", stdout, 0);
+        midlet.print("ELF not supported!", stdout, 0, scope);
 
         ITEM.put("status", new Double(69));
         return ITEM;
