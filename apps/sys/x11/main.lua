a81654587a486f0208d3f3b20807b649efd0474a
Mr Lima <felipebr4095@gmail.com>
2026-02-07 17:36:42 -0300

    x11

--- apps/sys/x11/main.lua
+++ apps/sys/x11/main.lua
@@ -3,9 +3,19 @@
 os.setproc("name", "x11")
 
 if arg[1] == "screen" then
-elseif arg[1] == "list" then   
-elseif arg[1] == "quest" then   
-elseif arg[1] == "edit" then   
+elseif arg[1] == "list" then
+    if arg[2] then
+        local file = io.open(os.join(arg[2]))
+        if file then
+        
+        else
+            print()
+        end
+    else
+        print("x11: list: missing file")
+    end
+elseif arg[1] == "quest" then
+elseif arg[1] == "edit" then
 elseif arg[1] == "version" then
 else
     local screen = graphics.new("screen", "X.Org")
