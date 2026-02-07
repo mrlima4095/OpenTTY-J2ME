8218e221f32bc4a5f7037a6bcf9feb1b8baf1f5d
Mr Lima <felipebr4095@gmail.com>
2026-02-07 15:20:12 -0300

    /bin/stop

---
+++ apps/sys/stop.lua
@@ -0,0 +1,22 @@
+#!/bin/lua
+
+if arg[1] then
+    local libcore = require("libcore")
+
+    for i = 1, #arg - 1 do
+        local pattern = arg[i]
+        local sys = os.getproc()
+        
+        for k,v in pairs(sys) do
+            if v == pattern then
+                local status = os.request(1, "sendsig", { pid = k, signal = 9 })
+                if status > 0 then
+                    print("stop: " .. arg[i] .. ": " .. libcore.errormsg(status))
+                    os.exit(tonumber(status))
+                end
+            end
+        end
+    end
+else
+    print("stop: usage: stop [name...]")
+end
