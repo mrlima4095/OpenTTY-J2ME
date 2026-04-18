d358253aba6443fef1e05626b4817dc044004b97
Mr Lima <felipebr4095@gmail.com>
2026-04-18 13:11:29 -0300

    Double

--- src/Lua.java
+++ src/Lua.java
@@ -2881,7 +2881,7 @@ public class Lua {
         public void run() { if (root instanceof LuaFunction) { Vector arg = new Vector(); try { ((LuaFunction) root).call(arg); } catch (Throwable e) { midlet.print(midlet.getCatch(e), stdout, id, father); } } }
 
         public Double exec(Vector args) throws Exception {
-            if (args.isEmpty()) { return gotbad(1, "execute", "string expected, got no value"); }
+            if (args.isEmpty()) { return (Double) gotbad(1, "execute", "string expected, got no value"); }
             else {
                 String command = toLuaString(args.elementAt(0));
                 String mainCommand = midlet.getCommand(command), argument = midlet.getArgument(command);
