a34d0a0b2199ccb53eb9a712623d6f9375849835
Lima <felipebr4095@gmail.com>
2026-04-25 18:45:29 -0300

    update Lua.java

--- src/Lua.java
+++ src/Lua.java
@@ -3011,7 +3011,7 @@ public class Lua {
                 else if (mainCommand.equals("build")) {
                     Process process = new Process(midlet, ("build " + args[0]).trim(), midlet.joinpath(program, scope), midlet.getUser(owner), owner, pid, out, scope, -1);
                     midlet.sys.put(pid, process);
-                    process.c.run(program, code, new Hashtable);
+                    process.c.run(program, code, new Hashtable());
                 }
                 else if (mainCommand.equals("builtin") || mainCommand.equals("command")) { Vector payload = new Vector(); payload.addElement(argument); payload.addElement(true); payload.addElement(FALSE); return exec(payload); }
                 else if (mainCommand.equals("false")) { status = 255; }
