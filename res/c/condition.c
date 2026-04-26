4ebcbad82061e6585fe2439d7e22cf29d102f946
Mr Lima <felipebr4095@gmail.com>
2026-04-25 23:10:08 -0300

    condition

---
+++ res/c/condition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 18;
+    
+    if (idade >= 18) {
+        printf("Maior de idade\n");
+    } else {
+        printf("Menor de idade\n");
+    }
+    return 0;
+}
