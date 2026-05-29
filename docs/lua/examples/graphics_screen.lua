458297b6043d127cc974bc88f0ab2437e9a184ae
Mr Lima <felipebr4095@gmail.com>
2026-05-29 19:41:08 -0300

    Creating and Reading screens

--- docs/lua/examples/graphics_screen.lua
+++ docs/lua/examples/graphics_screen.lua
@@ -1,2 +1,5 @@
 -- Lua J2ME - Code examples
 -- This is an example of writing a file content with Lua API
+
+local previous = graphics.getCurrent() -- Get current screen
+local screen = graphics.new("screen", "Screen Title") -- Create Screen Object
