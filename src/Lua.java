59f8c7e4a0fdfc8dfe8402a02b0b35a16749021d
Mr Lima <felipebr4095@gmail.com>
2026-05-31 13:06:09 -0300

    re if

--- src/Lua.java
+++ src/Lua.java
@@ -2806,7 +2806,7 @@ public class Lua {
                             break;
 
                 }
-                else if (mainCommand.equals("whoami")) { midlet.print((String) father.get("USER"), output, id, father); }
+                if (mainCommand.equals("whoami")) { midlet.print((String) father.get("USER"), output, id, father); }
                 else if (mainCommand.equals("id")) {
                     if (args.length == 0) {
                         midlet.print("uid=" + id + "(" + midlet.getUser(id) + ")", output, id, father);
