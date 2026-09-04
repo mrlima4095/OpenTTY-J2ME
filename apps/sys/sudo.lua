ad592c2f266ea3ccbf4baa6eb6336a3a41718515
Mr Lima <felipebr4095@gmail.com>
2026-09-03 21:41:24 -0300

    no regs

--- apps/sys/sudo.lua
+++ apps/sys/sudo.lua
@@ -5,7 +5,6 @@ if arg[1] then
     local screen = graphics.new("screen", "OpenTTY " .. getAppProperty("MIDlet-Version"))
     local back = graphics.new("command", { label = "Back", type = "back", priority = 1 })
     local run = graphics.new("command", { label = "Run", type = "screen", priority = 1 })
-    local switch = graphics.new("command", { label = "Switch to...", type = "screen", priority = 2 })
     
     graphics.append(screen, { type = "field", label = "[sudo] password for " .. java.midlet.username, value = "", mode = "password" })
     graphics.addCommand(screen, back)
@@ -40,8 +39,7 @@ if arg[1] then
                 print("Permission denied!")
                 os.exit(13)
             end
-        end,
-        [switch] = graphics.taskmngr
+        end
     })
     
     os.setproc("screen", screen)
