7343a62e81157886646f02154be06ad99b41fd79
Mr Lima <felipebr4095@gmail.com>
2026-09-03 21:34:17 -0300

    x11 warn: add Switch to... button and register screen for task manager

--- apps/sys/x11/warn.lua
+++ apps/sys/x11/warn.lua
@@ -1,7 +1,12 @@
 #!/bin/lua
 
 if arg[1] and arg[2] then
-    graphics.display(graphics.new("alert", arg[1], arg[2]))
+    local alert = graphics.new("alert", arg[1], arg[2])
+    local switch = graphics.new("command", { label = "Switch to...", type = "screen", priority = 2 })
+    graphics.addCommand(alert, switch)
+    graphics.handler(alert, { [switch] = graphics.taskmngr })
+    os.setproc("screen", alert)
+    graphics.display(alert)
 else
     print("warn [title] [body]")
     print("- display an alert window")
