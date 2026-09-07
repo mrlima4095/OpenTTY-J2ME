a3aae24cc1e6ea342a5cd3168b2914af16135e8d
Mr Lima <felipebr4095@gmail.com>
2026-09-07 12:48:42 -0300

    new

--- src/Lua.java
+++ src/Lua.java
@@ -3106,7 +3106,7 @@ public class Lua {
 
                     midlet.print("at " + (System.currentTimeMillis() - before), output, id, father);
                 }
-                else if (mainCommand.equals("date")) { midlet.print(java.util.Date().toString(), output, id, father); }
+                else if (mainCommand.equals("date")) { midlet.print(new java.util.Date().toString(), output, id, father); }
                 else if (mainCommand.equals("whoami")) { midlet.print((String) father.get("USER"), output, id, father); }
                 else if (mainCommand.equals("id")) {
                     if (args.length == 0) {
