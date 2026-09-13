17ce3acdacafcc095356707817a0e738a9b459eb
Mr Lima <felipebr4095@gmail.com>
2026-09-13 15:05:39 -0300

    install mkroot

--- apps/1.18.2/sys/install.lua
+++ apps/1.18.2/sys/install.lua
@@ -8,6 +8,8 @@ if arg[1] then
         os.exit(2)
     end
 
+    os.mkdir(root)
+
     local dirs = { "bin/", "boot/", "dev/", "etc/", "home/", "lib/", "mnt/", "root/", "proc/", "tmp/" }
     for _, dir in pairs(dirs) do
         local dir_pwd = root .. dir
