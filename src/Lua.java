5f2a11c7b2e34c698931f775155f3ead5056e1e0
Mr Lima <felipebr4095@gmail.com>
2026-09-20 15:14:06 -0300

    lua: close internal io.copy streams

--- src/Lua.java
+++ src/Lua.java
@@ -1922,11 +1922,12 @@ public class Lua {
                             
                             InputStream is = midlet.getInputStream(file, father);
                             if (is == null) { return new Double(127); }
-                            
-                            byte[] buffer = new byte[1024];
-                            int bytesRead;
-                            while ((bytesRead = is.read(buffer)) != -1) { os.write(buffer, 0, bytesRead); }
-                            os.flush(); is.close();
+                            try {
+                                byte[] buffer = new byte[1024];
+                                int bytesRead;
+                                while ((bytesRead = is.read(buffer)) != -1) { os.write(buffer, 0, bytesRead); }
+                                os.flush();
+                            } finally { try { is.close(); } catch (Exception e) { } }
                             return new Double(0);
                         }
                         else if (target instanceof StringBuffer || target instanceof String) {
@@ -1935,13 +1936,13 @@ public class Lua {
                                 ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
                                 InputStream is = midlet.getInputStream(file, father);
-                                if (is == null) { return new Double(127); }
-
-                                byte[] buffer = new byte[1024];
-                                int bytesRead;
-                                while ((bytesRead = is.read(buffer)) != -1) { baos.write(buffer, 0, bytesRead); }
-
-                                return new Double(midlet.write(toLuaString(target), baos.toByteArray(), id, father));
+                                if (is == null) { baos.close(); return new Double(127); }
+                                try {
+                                    byte[] buffer = new byte[1024];
+                                    int bytesRead;
+                                    while ((bytesRead = is.read(buffer)) != -1) { baos.write(buffer, 0, bytesRead); }
+                                    return new Double(midlet.write(toLuaString(target), baos.toByteArray(), id, father));
+                                } finally { try { is.close(); } catch (Exception e) { } try { baos.close(); } catch (Exception e) { } }
                             }
                         }
                     }
